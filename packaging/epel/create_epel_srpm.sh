dd3dc7bd318967f6a37d22e9eb822dd996bf333d
Michal Bocek <mbocek@redhat.com>
2019-12-20 17:02:33 +0100

    Add Makefile for Fedora Copr

--- packaging/epel/create_epel_srpm.sh
+++ packaging/epel/create_epel_srpm.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
 cleanup() {
-  rm -f convert2rhel.spec
-  rm -rf dist/
+  rm -f ${REPO_ROOT}/convert2rhel.spec
+  rm -rf ${REPO_ROOT}/dist/
   popd
 }
 
-BASEDIR=$(dirname "$0")
-pushd ${BASEDIR}/../..
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+REPO_ROOT=${SCRIPTPATH}/../..
+pushd ${REPO_ROOT}
 
 rm -rf dist/ SRPMS/
 python2 setup.py sdist
 
-cp ${BASEDIR}/convert2rhel.spec convert2rhel.spec
+cp ${SCRIPTPATH}/convert2rhel.spec convert2rhel.spec
 rpmlint convert2rhel.spec
 
 TIMESTAMP=`date +%Y%m%d%H%MZ -u`
@@ -20,8 +21,8 @@ GIT_BRANCH=`git rev-parse --abbrev-ref HEAD`
 sed -i "s/1%{?dist}/0.${TIMESTAMP}.${GIT_BRANCH}/g" convert2rhel.spec
 
 rpmbuild -bs convert2rhel.spec --define "debug_package %{nil}" \
-    --define "_sourcedir `pwd`/dist" \
-    --define "_srcrpmdir `pwd`/SRPMS" || { cleanup; exit 1; }
+    --define "_sourcedir ${REPO_ROOT}/dist" \
+    --define "_srcrpmdir ${REPO_ROOT}/SRPMS" || { cleanup; exit 1; }
 
 rpmlint SRPMS/convert2rhel*.rpm
 
