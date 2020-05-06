5d03a687b34e4b90b0ce8b1ac6b736cd21d2196c
Fellipe Henrique <fpedrosa@redhat.com>
2020-05-06 14:53:38 -0300

    Fix rpm build script on CentOS
    Changes on rpm build script to reflect the new project schema
    
    Signed-off-by: Fellipe Henrique <fpedrosa@redhat.com>

--- packaging/centos_ol/build_locally_on_rhel6_or_7.sh
+++ packaging/centos_ol/build_locally_on_rhel6_or_7.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # Run this script on CentOS/OL/RHEL 6 or 7 in the root of the cloned convert2rhel repo
+pushd ../../
 python setup.py sdist
 cp dist/* ~/rpmbuild/SOURCES
-rpmlint convert2rhel.spec
-rpmbuild -ba convert2rhel.spec --define "debug_package %{nil}"
-rpmlint ~/rpmbuild/RPMS/x86_64/convert2rhel*.rpm
+rpmlint packaging/centos_ol/convert2rhel.spec
+rpmbuild -ba packaging/centos_ol/convert2rhel.spec --define "debug_package %{nil}"
+rpmlint ~/rpmbuild/RPMS/noarch/convert2rhel*.rpm
+popd
