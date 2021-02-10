d98a94020b16c6f47a92ad17fc2f1d276f7d0c54
Michal Bocek <mbocek@redhat.com>
2021-02-10 21:50:40 +0100

    Bump to version 0.17
    Changes in this version include:
    - Fix broken package backup causing an incomplete rollback
    - Fix dependency issue when force replacing same-version kernel
    - Allow using RHSM repos when downloading same-version kernel

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ def read(fname):
 
 setup(
     name='convert2rhel',
-    version='0.16',
+    version='0.17',
     description='Automates the conversion of Red Hat Enterprise Linux'
                 ' derivative distributions to Red Hat Enterprise Linux.',
     long_description=read('README.md'),
