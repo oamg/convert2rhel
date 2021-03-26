7fa0ee080b1b1a2e27a85fd3b2ef4a8fd8940d3b
zhukovgreen <iam@zhukovgreen.pro>
2021-03-26 17:47:33 +0100

    Add pretend_centos7 and generalize solution

--- convert2rhel/unit_tests/conftest.py
+++ convert2rhel/unit_tests/conftest.py
@@ -73,18 +73,29 @@ def setup_logger(tmpdir):
     initialize_logger(log_name="convert2rhel", log_dir=tmpdir)
 
 
-@pytest.fixture()
-def _replace_data_dir_for_centos8(monkeypatch, pkg_root):
-    monkeypatch.setattr(
-        utils,
-        "DATA_DIR",
-        value=str(pkg_root / "convert2rhel/data/8/x86_64/"),
-    )
+def _replace_data_dir_base(system_version_num):
+    """Factory function serves as a base to mokeypatch utils.DATA_DIR."""
+
+    def fixture(monkeypatch, pkg_root):
+        monkeypatch.setattr(
+            utils,
+            "DATA_DIR",
+            value=str(
+                pkg_root
+                / ("convert2rhel/data/%s/x86_64/" % system_version_num)
+            ),
+        )
+
+    return fixture
 
 
-@pytest.fixture()
-def pretend_centos8(monkeypatch, _replace_data_dir_for_centos8):
-    # TODO create similar for rest systems and document its usage
+_replace_data_dir_for_centos8 = pytest.fixture(_replace_data_dir_base("8"))
+_replace_data_dir_for_centos7 = pytest.fixture(_replace_data_dir_base("7"))
+
+
+def _pretend_fixture_base(monkeypatch, system_release_version):
+    """Apply common logic in pretend_{os_name} fixtures."""
+
     monkeypatch.setattr(
         redhatrelease,
         "get_system_release_filepath",
@@ -93,7 +104,34 @@ def pretend_centos8(monkeypatch, _replace_data_dir_for_centos8):
     monkeypatch.setattr(
         utils,
         "get_file_content",
-        value=lambda _: "CentOS Linux release 8.3.2011",
+        value=lambda _: "CentOS Linux release %s" % system_release_version,
     )
     tool_opts.no_rpm_va = True
     system_info.resolve_system_info()
+
+
+def _pretend_centos_base(system_release_version):
+    """Factory function serves as a base to pretend_{os} fixtures."""
+    def fixture_centos7(monkeypatch, _replace_data_dir_for_centos7):
+        _pretend_fixture_base(monkeypatch, system_release_version)
+
+    def fixture_centos8(monkeypatch, _replace_data_dir_for_centos8):
+        _pretend_fixture_base(monkeypatch, system_release_version)
+
+    release2data_dir_relations = {
+        "8.3.2011": fixture_centos8,
+        "7.9.2009": fixture_centos7,
+    }
+
+    try:
+        return release2data_dir_relations[system_release_version]
+    except KeyError:
+        raise KeyError(
+            "Unknown system release version %s.\n"
+            "Available are: %s"
+            % (system_release_version, " ".join(release2data_dir_relations)),
+        )
+
+
+pretend_centos8 = pytest.fixture(_pretend_centos_base("8.3.2011"))
+pretend_centos7 = pytest.fixture(_pretend_centos_base("7.9.2009"))
