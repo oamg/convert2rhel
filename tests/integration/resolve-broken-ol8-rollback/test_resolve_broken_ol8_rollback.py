137241580ae08438ba8c58c8f69e789e7b7fd753
zhukovgreen <iam@zhukovgreen.pro>
2021-05-10 20:34:42 +0200

    Reformat tests to use new fixture

--- tests/integration/resolve-broken-ol8-rollback/test_resolve_broken_ol8_rollback.py
+++ tests/integration/resolve-broken-ol8-rollback/test_resolve_broken_ol8_rollback.py
@@ -5,18 +5,19 @@ import pexpect
 from envparse import env
 
 
-def test_proper_rhsm_clean_up(shell):
+def test_proper_rhsm_clean_up(shell, convert2rhel):
     """Test that c2r does not remove usermod and rhn-setup during rollback."""
 
     # Ensure usermode and rhn-setup packages are presented
     assert shell("yum install -y usermode rhn-setup").returncode == 0
 
     # run c2r until subscribing the system and then emulate pressing Ctrl + C
-    c2r = pexpect.spawn(
+    with convert2rhel(
         (
-            "convert2rhel "
-            "--serverurl {} --username {} "
-            "--password {} --pool {} "
+            "--serverurl {} "
+            "--username {} "
+            "--password {} "
+            "--pool {} "
             "--debug "
             "--no-rpm-va"
         ).format(
@@ -24,19 +25,15 @@ def test_proper_rhsm_clean_up(shell):
             env.str("RHSM_USERNAME"),
             env.str("RHSM_PASSWORD"),
             env.str("RHSM_POOL"),
-        ),
-        encoding="utf-8",
-        timeout=5 * 60,
-    )
-    c2r.logfile_read = sys.stdout
-    c2r.expect("Continue with the system conversion?")
-    c2r.sendline("y")
-    c2r.expect("Continue with the system conversion?")
-    c2r.sendline("y")
-    c2r.expect("Building subscription-manager command")
-    # send Ctrl-C
-    c2r.send(chr(3))
-    c2r.expect(pexpect.EOF)
+        )
+    ) as c2r:
+        c2r.expect("Continue with the system conversion?")
+        c2r.sendline("y")
+        c2r.expect("Continue with the system conversion?")
+        c2r.sendline("y")
+        c2r.expect("Building subscription-manager command")
+        # send Ctrl-C
+        c2r.send(chr(3))
 
     # check that packages still are in place
     assert shell("rpm -qi usermode").returncode == 0
