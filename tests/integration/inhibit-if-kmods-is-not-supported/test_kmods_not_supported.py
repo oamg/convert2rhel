db19e3064ddcfbb95fa629141f6f5e4bdcfcf8c7
zhukovgreen <iam@zhukovgreen.pro>
2021-05-13 09:08:32 +0200

    Add kmods-inhibitor int tests

---
+++ tests/integration/inhibit-if-kmods-is-not-supported/test_kmods_not_supported.py
@@ -0,0 +1,62 @@
+from pathlib import Path
+
+import pytest
+
+from envparse import env
+
+
+@pytest.mark.good_tests
+def test_good_convertion(convert2rhel):
+    with convert2rhel(
+        (
+            "-y "
+            "--no-rpm-va "
+            "--serverurl {} --username {} "
+            "--password {} --pool {} "
+            "--debug"
+        ).format(
+            env.str("RHSM_SERVER_URL"),
+            env.str("RHSM_USERNAME"),
+            env.str("RHSM_PASSWORD"),
+            env.str("RHSM_POOL"),
+        )
+    ) as c2r:
+        c2r.expect("Kernel modules are compatible.")
+    assert c2r.exitstatus == 0
+
+
+@pytest.fixture()
+def insert_custom_kmod(shell):
+    def factory():
+        origin_kmod_loc = Path(
+            "/lib/modules/$(uname -r)/"
+            "kernel/drivers/net/bonding/bonding.ko.xz"
+        )
+        new_kmod_dir = origin_kmod_loc.parent / "custom_module_location"
+
+        shell(f"mkdir {new_kmod_dir.as_posix()}")
+        shell(f"mv {origin_kmod_loc.as_posix()} {new_kmod_dir.as_posix()}")
+        shell(f"insmod {(new_kmod_dir / origin_kmod_loc.name).as_posix()}")
+
+    return factory
+
+
+@pytest.mark.bad_tests
+def test_bad_convertion(shell, insert_custom_kmod, convert2rhel):
+    insert_custom_kmod()
+    with convert2rhel(
+        (
+            "-y "
+            "--no-rpm-va "
+            "--serverurl {} --username {} "
+            "--password {} --pool {} "
+            "--debug"
+        ).format(
+            env.str("RHSM_SERVER_URL"),
+            env.str("RHSM_USERNAME"),
+            env.str("RHSM_PASSWORD"),
+            env.str("RHSM_POOL"),
+        )
+    ) as c2r:
+        c2r.expect("The following kernel modules are not supported in RHEL")
+    assert c2r.exitstatus == 1
