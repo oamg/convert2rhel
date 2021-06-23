baf92f0a6e78d6badc0d1ec2f6c5461480ad94e9
Michal Bocek <mbocek@redhat.com>
2021-06-23 23:12:47 +0200

    Add integration tests for offline conversions
    Test that air-gapped systems connected only to the Satellite can be
    converted.

---
+++ tests/integration/tier1/convert-offline-systems/run_conversion.py
@@ -0,0 +1,17 @@
+import os
+
+from envparse import env
+
+
+def test_convert_offline_systems(convert2rhel):
+    """Test converting systems not connected to the Internet but requiring sub-mgr (e.g. managed by Satellite)."""
+
+    os.environ["CONVERT2RHEL_UNSUPPORTED_INCOMPLETE_ROLLBACK"] = "1"
+    with convert2rhel(
+        ("-y --no-rpm-va -k {} -o {} --keep-rhsm --debug").format(
+            env.str("SATELLITE_KEY"),
+            env.str("SATELLITE_ORG"),
+        )
+    ) as c2r:
+        c2r.expect("Conversion successful!")
+    assert c2r.exitstatus == 0
