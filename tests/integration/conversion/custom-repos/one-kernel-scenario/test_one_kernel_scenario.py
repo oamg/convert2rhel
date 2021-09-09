17c96ee93118685fb48aacd9b3cfe05fcc0219b5
Martin Kluson <mkluson@redhat.com>
2021-09-09 14:51:10 +0200

    Arange tests in tiers
    Rearanging the test plans to follow tiers structure like this:
    
    - tier0 = tests running on source system (the system is not converted)
    - tier1 = tests running on target system (the system is converted)
    - tier2 = tests of other components in convert workflow
    
    - Disable broken rollback test fot centos-7

--- tests/integration/conversion/custom-repos/one-kernel-scenario/test_one_kernel_scenario.py
+++
@@ -1,7 +0,0 @@
-def test_one_kernel_scenario(shell):
-    os_release = shell("cat /etc/os-release").output
-    assert "Red Hat Enterprise Linux" in os_release
-
-    # Check if kernel is RHEL one
-    kernel = shell("rpm -q --qf '%{NAME} %{VERSION}-%{RELEASE} %{VENDOR}\n' kernel")
-    assert "Red Hat" in kernel
