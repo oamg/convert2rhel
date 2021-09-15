75b987ea34be56fc6cae1f7adb17ca91a9b4ddbe
Martin Litwora <mlitwora@redhat.com>
2021-09-15 11:52:36 +0200

    Check that system is unregistered during rollback

--- tests/integration/tier0/resolve-broken-ol8-rollback/test_resolve_broken_ol8_rollback.py
+++ tests/integration/tier0/resolve-broken-ol8-rollback/test_resolve_broken_ol8_rollback.py
@@ -7,7 +7,9 @@ from envparse import env
 
 
 def test_proper_rhsm_clean_up(shell, convert2rhel):
-    """Test that c2r does not remove usermod and rhn-setup during rollback."""
+    """Test that c2r does not remove usermod, rhn-setup and os-release during rollback.
+    Also checks that the system was succesfully unregistered.
+    """
 
     # Ensure usermode and rhn-setup packages are presented
     assert shell("yum install -y usermode rhn-setup").returncode == 0
@@ -25,9 +27,12 @@ def test_proper_rhsm_clean_up(shell, convert2rhel):
         c2r.sendline("y")
         c2r.expect("Continue with the system conversion?")
         c2r.sendline("y")
-        c2r.expect("Building subscription-manager command")
-        # send Ctrl-C
-        c2r.send(chr(3))
+        c2r.expect("Continue with the system conversion?")
+        c2r.sendline("y")
+        c2r.expect("The tool allows rollback of any action until this point.")
+        c2r.sendline("n")
+        c2r.expect("Calling command 'subscription-manager unregister'")
+        c2r.expect("System unregistered successfully.")
 
     # check that packages still are in place
     assert shell("rpm -qi usermode").returncode == 0
