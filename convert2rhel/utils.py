026b54e9f691c56d09e319d8b3a2b9d919fdeafa
Andrew-ang9 <91086569+Andrew-ang9@users.noreply.github.com>
2021-10-21 15:46:30 -0400

    Halt when unable to download backup when getting packages
    I changed what happens when the conversion can't rollback the rpm files.
    It now halts the conversion and shows them the packages that couldn't be
    rolledback and then lets the use know to check that their packages are up to
    date and try the conversion again and to use an envar to bypass the check
    
    Fixes OAMG-5463

--- convert2rhel/utils.py
+++ convert2rhel/utils.py
@@ -472,10 +472,19 @@ def download_pkg(
     output, ret_code = run_cmd_in_pty(cmd, print_output=False)
     if ret_code != 0:
         loggerinst.warning(
-            "Couldn't download the %s package using yumdownloader.\n"
-            "Output from the yumdownloader call:\n%s" % (pkg, output)
+            " you are using an env that overrides an inhibitor and"
+            " you understand that if you continue you might lose the rollback."
+            " \nOutput from the yum downloader call:\n%s" % (pkg, output)
         )
-        return None
+
+        if not CONVERT2RHEL_UNSUPPORTED_INCOMPLETE_ROLLBACK in os.environ:  # template for what needs to be done
+            loggerinst.critical("")
+        loggerinst.info(
+            "could not complete the rollback because one or more files are not updated, Check to see"
+            "if all packages are updated and try the conversion again"
+        )
+
+        return none
 
     path = get_rpm_path_from_yumdownloader_output(cmd, output, dest)
     if path:
