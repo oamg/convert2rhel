7920fc401fd73cf191b9d298d2fe8abde1142dc3
Martin Litwora <mlitwora@redhat.com>
2021-11-22 16:52:34 +0100

    Enable Oracle Linux tests for CI

--- tests/integration/tier0/inhibit-if-oracle-system-uses-not-standard-kernel/test_oracle_bad_kernel.py
+++ tests/integration/tier0/inhibit-if-oracle-system-uses-not-standard-kernel/test_oracle_bad_kernel.py
@@ -9,21 +9,6 @@ except ImportError:
 from envparse import env
 
 
-@pytest.mark.good_tests
-def test_good_conversion(convert2rhel):
-    with convert2rhel(
-        ("-y --no-rpm-va --serverurl {} --username {} --password {} --pool {} --debug").format(
-            env.str("RHSM_SERVER_URL"),
-            env.str("RHSM_USERNAME"),
-            env.str("RHSM_PASSWORD"),
-            env.str("RHSM_POOL"),
-        )
-    ) as c2r:
-        c2r.expect("Kernel is compatible with RHEL")
-    assert c2r.exitstatus == 0
-
-
-@pytest.mark.bad_tests
 def test_bad_conversion(convert2rhel):
     with convert2rhel(
         ("-y --no-rpm-va --serverurl {} --username {} --password {} --pool {} --debug").format(
