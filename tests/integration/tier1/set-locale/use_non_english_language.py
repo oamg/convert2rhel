e112d395310517281c0b95aa9c92d91673c624ef
Martin Litwora <mlitwora@redhat.com>
2021-12-06 16:50:25 +0100

    Add int test that sets non english LANG

---
+++ tests/integration/tier1/set-locale/use_non_english_language.py
@@ -0,0 +1,17 @@
+import platform
+
+
+def test_use_non_english_language(shell):
+    """
+    We need to test the ability of convert2rhel to convert the system when non english
+    language is set (using non ascii language).
+    Ref bug: https://bugzilla.redhat.com/show_bug.cgi?id=2022854
+    """
+    # install Chinese language pack for CentOS-8 and Oracle Linux 8
+    os = platform.platform()
+    if os.find("centos-8") != -1 or os.find("oracle-8") != -1:
+        assert shell("dnf install glibc-langpack-zh -y").returncode == 0
+
+    # set LANG to Chinese
+    assert shell("localectl list-locales | grep zh_CN.utf8").returncode == 0
+    assert shell("localectl set-locale LANG=zh_CN.utf8").returncode == 0
