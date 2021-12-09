bda816d2923189c72f1ebb6bfa69e33407764ed0
Martin Litwora <mlitwora@redhat.com>
2021-12-09 21:37:12 +0100

    enhance test for ol7/8

--- tests/integration/tier1/yum-distro-sync/install_problematic_package.py
+++ tests/integration/tier1/yum-distro-sync/install_problematic_package.py
@@ -1,3 +1,6 @@
+import platform
+
+
 def test_install_problematic_package(shell):
     """Need to install a package, which is in CentOS repos (which we are converting) and is not present in RHEL
     repositories. The package in question (cpaste) is from the CentOS Extras repo which is enabled by default on
@@ -5,4 +8,11 @@ def test_install_problematic_package(shell):
 
     When the test starts failing we will need to change the selected package."""
 
+    # On artemis images exists packages that needs to be removed so `yum distro-sync` will fail on cpaste.
+    # Otherwise it would not fail (yum return 0 when at least one package is ok)
+    if platform.platform().find("centos-8") > 0:
+        assert (
+            shell("yum remove python2-pip python2-pip-wheel python2-setuptools python2-setuptools-wheel -y").returncode
+            == 0
+        )
     assert shell("yum install -y cpaste").returncode == 0
