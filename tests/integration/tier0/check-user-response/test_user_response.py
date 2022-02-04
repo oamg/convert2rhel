3e438ee45a1ddd16b1e8de64b6581c80c68c191d
danmyway <ddiblik@redhat.com>
2022-02-04 11:56:03 +0100

    Change assert, remove if

--- tests/integration/tier0/check-user-response/test_user_response.py
+++ tests/integration/tier0/check-user-response/test_user_response.py
@@ -5,8 +5,8 @@ def test_check_user_response(convert2rhel):
         c2r.expect(" ... activation key not found, username and password required")
         c2r.expect("Username: ")
         c2r.sendline()
-        if c2r.expect("Username: ") == 0:
-            c2r.sendcontrol("d")
+        assert c2r.expect("Username: ") == 0
+        c2r.sendcontrol("d")
     assert c2r.exitstatus != 0
 
     # Run c2r registration with username but no password
@@ -16,6 +16,6 @@ def test_check_user_response(convert2rhel):
         c2r.sendline("foo")
         c2r.expect("Password: ")
         c2r.sendline()
-        if c2r.expect("Password: ") == 0:
-            c2r.sendcontrol("d")
+        assert c2r.expect("Password: ") == 0
+        c2r.sendcontrol("d")
     assert c2r.exitstatus != 0
