1ede7081c1926395aafdeb642c9cf573f27c09a3
danmyway <ddiblik@redhat.com>
2022-02-07 11:17:19 +0100

    Add proper registration. Integration test to #390.

--- tests/integration/tier0/check-user-response/test_user_response.py
+++ tests/integration/tier0/check-user-response/test_user_response.py
@@ -1,21 +1,27 @@
+from envparse import env
+
+
 def test_check_user_response(convert2rhel):
 
-    # Run c2r registration with no username
-    with convert2rhel("-y " "--no-rpm-va") as c2r:
+    # Run c2r registration with no username and password provided
+    # check for user prompt enforcing input, then continue with registration
+    with convert2rhel(
+        "-y --no-rpm-va --serverurl {}".format(
+            env.str("RHSM_SERVER_URL"),
+        )
+    ) as c2r:
         c2r.expect(" ... activation key not found, username and password required")
         c2r.expect("Username: ")
         c2r.sendline()
         assert c2r.expect("Username: ") == 0
-        c2r.sendcontrol("d")
-    assert c2r.exitstatus != 0
-
-    # Run c2r registration with username but no password
-    with convert2rhel("-y " "--no-rpm-va") as c2r:
-        c2r.expect(" ... activation key not found, username and password required")
-        c2r.expect("Username: ")
-        c2r.sendline("foo")
+        # Provide username, expect password prompt
+        c2r.sendline(env.str("RHSM_USERNAME"))
         c2r.expect("Password: ")
         c2r.sendline()
         assert c2r.expect("Password: ") == 0
+        # Provide password, expect registration
+        c2r.sendline(env.str("RHSM_PASSWORD"))
+        assert c2r.expect("Registering the system using subscription-manager ...") == 0
+        assert c2r.expect("Enter number of the chosen subscription: ") == 0
         c2r.sendcontrol("d")
     assert c2r.exitstatus != 0
