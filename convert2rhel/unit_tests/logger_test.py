oamg/convert2rhel

commit b87046035499df3ad76909423af0db271077d231

Always log debug info to the log file
Now debug info is stored inside log file, whatever the user use or not
`--debug` argument.

Fixed when user not pass `--debug` information, the info was passed to
stdout, now these info only passed to the stdout if user add the debug
argument.

Signed-off-by: Fellipe Henrique <fpedrosa@redhat.com>

convert2rhel/logger.py
@@ -14,7 +14,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
-'''
+"""
 Customized logging functionality
 
 CRITICAL  (50)    Calls critical() function and sys.exit(1)
@@ -24,12 +24,15 @@ INFO      (20)    Prints info message (no date/time, just plain message)
 TASK      (15)    CUSTOM LABEL - Prints a task header message (using asterisks)
 DEBUG     (10)    Prints debug message (using date/time)
 FILE      (5)     CUSTOM LABEL - Prints only to file handler (using date/time)
-'''
+"""
+from convert2rhel.toolopts import tool_opts
 import logging
-import sys
 import os
+import sys
+from utils import format_msg_with_datetime
 
 LOG_DIR = "/var/log/convert2rhel"
+
 
 class LogLevelTask:
     level = 15
@@ -42,10 +45,10 @@ class LogLevelFile:
 
 
 def initialize_logger(log_name):
-    '''Initialize custom logging levels, handlers, and so on. Call this method
+    """Initialize custom logging levels, handlers, and so on. Call this method
     from your application's main start point.
         log_name = the name for the log file
-    '''
+    """
     # check if already initialized with custom class
     if logging.getLoggerClass() == CustomLogger:
         return
@@ -87,6 +90,7 @@ class CustomLogger(logging.Logger, object):
         class and causes 'TypeError: super() argument 1 must be type, not
         classobj' so we use multiple inheritance to get around the problem.
     """
+
     def task(self, msg, *args, **kwargs):
         super(CustomLogger, self).log(LogLevelTask.level, msg, *args,
                                       **kwargs)
@@ -104,7 +108,9 @@ class CustomLogger(logging.Logger, object):
         if tool_opts.debug:
             super(CustomLogger, self).debug(msg, *args, **kwargs)
         else:
-            pass
+            super(CustomLogger, self).log(LogLevelFile.level,
+                                          format_msg_with_datetime(msg, "debug"),
+                                          *args, **kwargs)
 
     def info(self, msg, *args, **kwargs):
         super(CustomLogger, self).info(msg, *args, **kwargs)
@@ -117,6 +123,7 @@ class CustomFormatter(logging.Formatter, object):
         class and causes 'TypeError: super() argument 1 must be type, not
         classobj' so we use multiple inheritance to get around the problem.
     """
+
     def format(self, record):
         if record.levelno == LogLevelTask.level:
             temp = '*' * (90 - len(record.msg) - 25)

convert2rhel/utils.py
@@ -15,6 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+import datetime
 import errno
 import getpass
 import inspect
@@ -44,6 +45,12 @@ class Color:
 DATA_DIR = "/usr/share/convert2rhel/"
 # Directory for temporary data to be stored during runtime
 TMP_DIR = "/tmp/convert2rhel/"
+
+
+def format_msg_with_datetime(msg, level):
+    """Return a string with msg formatted according to the level"""
+    temp_date = datetime.datetime.now().strftime("%m/%d/%Y %H:%M:%S")
+    return "[%s] %s - %s", temp_date, level.upper(), msg
 
 
 def get_executable_name():
@@ -220,6 +227,7 @@ def get_traceback_str():
 
 class DictWListValues(dict):
     """Python 2.4 replacement for Python 2.5+ collections.defaultdict(list)."""
+
     def __getitem__(self, item):
         if item not in self.iterkeys():
             self[item] = []
@@ -229,6 +237,7 @@ class DictWListValues(dict):
 
 class ChangedRPMPackagesController(object):
     """Keep control of installed/removed RPM pkgs for backup/restore."""
+
     def __init__(self):
         self.installed_pkgs = []
         self.removed_pkgs = []

convert2rhel/unit_tests/logger_test.py
@@ -122,4 +122,4 @@ class TestLogger(unittest.TestCase):
         self.assertEqual(logging.Handler.handle.called, 6)
         tool_opts.debug = False
         loggerinst.debug("debugmsg2")
-        self.assertEqual(logging.Handler.handle.called, 6)
+        self.assertEqual(logging.Handler.handle.called, 7)

commit ff36a0f88ecac01f5dea59e5a2608006fa296c3b

Fix lazy evaluation of string logs for loglevel.FILE
For log messages, you can do lazy evaluation of the format string to
occassionally boost performance when a log message is not used.  For
instance:

  class Hypothetical(object):
      def __str__(self):
          return self.calculate_fibonacci_10000()
  log.debug("10,000th fibonacci number is: %s", Hypothetical())

The format string will not be interpolated (and thus the 10,000th
fibonacci number will not be calculated) unless we are actually going to
log the debug() output.

The code in commit b870460 broke that because it calculated its own
format string and args for the message, interfering with whatever was
passed in via function parameter.  This change fixes that by moving the
formatting pieces of b870460 into the CustomFormatter class.  By that
time, the message string has already been interpolated so any changes to
the message string will not interfere.  Additionally, in the formatter
class, those pieces can be performed by standard logging.Formatter
features so we don't need the custom code to do that anymore.

OAMG-6571 #in-progress

convert2rhel/logger.py
@@ -33,7 +33,6 @@ import sys
 from time import gmtime, strftime
 
 from convert2rhel.toolopts import tool_opts
-from convert2rhel.utils import format_msg_with_datetime
 
 
 LOG_DIR = "/var/log/convert2rhel"
@@ -153,7 +152,7 @@ def _debug(self, msg, *args, **kwargs):
         if tool_opts.debug:
             self._log(logging.DEBUG, msg, args, **kwargs)
         else:
-            self._log(LogLevelFile.level, format_msg_with_datetime(msg, "debug"), args, **kwargs)
+            self._log(LogLevelFile.level, msg, args, **kwargs)
 
 
 class bcolors:
@@ -183,7 +182,7 @@ class CustomFormatter(logging.Formatter, object):
             new_fmt = fmt_orig if self.color_disabled else bcolors.OKGREEN + fmt_orig + bcolors.ENDC
             self._fmt = new_fmt
             self.datefmt = "%m/%d/%Y %H:%M:%S"
-        elif record.levelno in [logging.INFO, LogLevelFile.level]:
+        elif record.levelno in [logging.INFO]:
             self._fmt = "%(message)s"
             self.datefmt = ""
         elif record.levelno in [logging.WARNING]:

convert2rhel/utils.py
@@ -15,7 +15,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-import datetime
 import errno
 import getpass
 import inspect
@@ -55,12 +54,6 @@ DATA_DIR = "/usr/share/convert2rhel/"
 # Directory for temporary data to be stored during runtime
 TMP_DIR = "/var/lib/convert2rhel/"
 BACKUP_DIR = os.path.join(TMP_DIR, "backup")
-
-
-def format_msg_with_datetime(msg, level):
-    """Return a string with msg formatted according to the level"""
-    temp_date = datetime.datetime.now().strftime("%m/%d/%Y %H:%M:%S")
-    return "[%s] %s - %s", temp_date, level.upper(), msg
 
 
 def get_executable_name():

convert2rhel/unit_tests/logger_test.py
@@ -33,18 +33,18 @@ def test_logger_handlers(tmpdir, caplog, read_std, is_py2, capsys):
     logger = logging.getLogger(__name__)
 
     # emitting some log entries
-    logger.info("Test info")
-    logger.debug("Test debug")
+    logger.info("Test info: %s", "data")
+    logger.debug("Test debug: %s", "other data")
 
     # Test if logs were emmited to the file
     with open(str(tmpdir.join(log_fname))) as log_f:
-        assert "Test info" in log_f.readline().rstrip()
-        assert "Test debug" in log_f.readline().rstrip()
+        assert "Test info: data" in log_f.readline().rstrip()
+        assert "Test debug: other data" in log_f.readline().rstrip()
 
     # Test if logs were emmited to the stdout
     stdouterr_out, stdouterr_err = read_std()
-    assert "Test info" in stdouterr_out
-    assert "Test debug" in stdouterr_out
+    assert "Test info: data" in stdouterr_out
+    assert "Test debug: other data" in stdouterr_out
 
 
 def test_tools_opts_debug(tmpdir, read_std, is_py2):
@@ -52,22 +52,22 @@ def test_tools_opts_debug(tmpdir, read_std, is_py2):
     logger_module.setup_logger_handler(log_name=log_fname, log_dir=str(tmpdir))
     logger = logging.getLogger(__name__)
     tool_opts.debug = True
-    logger.debug("debug entry 1")
+    logger.debug("debug entry 1: %s", "data")
     stdouterr_out, stdouterr_err = read_std()
     # TODO should be in stdout, but this only works when running this test
     #   alone (see https://github.com/pytest-dev/pytest/issues/5502)
     try:
-        assert "debug entry 1" in stdouterr_out
+        assert "debug entry 1: data" in stdouterr_out
     except AssertionError:
         if not is_py2:
-            assert "debug entry 1" in stdouterr_err
+            assert "debug entry 1: data" in stdouterr_err
         else:
             # this workaround is not working for py2 - passing
             pass
     tool_opts.debug = False
-    logger.debug("debug entry 2")
+    logger.debug("debug entry 2: %s", "data")
     stdouterr_out, stdouterr_err = read_std()
-    assert "debug entry 2" not in stdouterr_out
+    assert "debug entry 2: data" not in stdouterr_out
 
 
 def test_logger_custom_logger(tmpdir, caplog):
@@ -75,10 +75,15 @@ def test_logger_custom_logger(tmpdir, caplog):
     log_fname = "convert2rhel.log"
     logger_module.setup_logger_handler(log_name=log_fname, log_dir=str(tmpdir))
     logger = logging.getLogger(__name__)
-    logger.task("Some task")
-    logger.file("Some task write to file")
+    logger.task("Some task: %s", "data")
+    logger.file("Some task write to file: %s", "data")
     with pytest.raises(SystemExit):
-        logger.critical("Critical error")
+        logger.critical("Critical error: %s", "data")
+
+    assert len(caplog.records) == 3
+    assert "Some task: data\n" in caplog.text
+    assert "Some task write to file: data\n" in caplog.text
+    assert "Critical error: data\n" in caplog.text
 
 
 @pytest.mark.parametrize(