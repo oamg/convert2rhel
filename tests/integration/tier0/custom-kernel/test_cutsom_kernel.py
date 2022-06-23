f0ccde5b9f76f956226405e56c1991a8b3416706
Martin Litwora <mlitwora@redhat.com>
2022-06-23 11:53:04 +0200

    Add Integration test to ckeck the presence of the custom kernel installed

---
+++ tests/integration/tier0/custom-kernel/test_cutsom_kernel.py
@@ -0,0 +1,20 @@
+import platform
+
+
+def test_custom_kernel(convert2rhel):
+    """
+
+    Run the conversion with custom kernel installed on the system.
+
+    """
+    system_version = platform.platform()
+
+    if "centos-7" in system_version:
+        string = "CentOS"
+    elif "oracle-7" in system_version:
+        string = "Oracle"
+
+    with convert2rhel(("--no-rpm-va --debug")) as c2r:
+        c2r.expect("WARNING - Custom kernel detected. The booted kernel needs to be signed by {}".format(string))
+        c2r.expect("CRITICAL - The booted kernel version is incompatible with the standard RHEL kernel.")
+    assert c2r.exitstatus != 0
