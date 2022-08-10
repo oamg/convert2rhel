7af9a7ca9c4106e082e6efd869e657042e70638f
Rodolfo Olivieri <rolivier@redhat.com>
2022-08-10 10:04:46 -0300

    Apply patch from 3b6146168eb19ea2cb2199b02b37ee2c1ee3cb5b
    Signed-off-by: Rodolfo Olivieri <rolivier@redhat.com>

--- tests/integration/tier0/inhibit-if-kmods-is-not-supported/test_kmods_not_supported.py
+++ tests/integration/tier0/inhibit-if-kmods-is-not-supported/test_kmods_not_supported.py
@@ -1,4 +1,3 @@
-import os
 import platform
 
 from pathlib import Path
@@ -13,6 +12,11 @@ system_version = platform.platform()
 
 @pytest.fixture()
 def insert_custom_kmod(shell):
+    """
+    Move the kernel module to custom location, so it is marked as custom.
+    Then load the kernel module.
+    """
+
     def factory():
         origin_kmod_loc = Path("/lib/modules/$(uname -r)/kernel/drivers/net/bonding/bonding.ko.xz")
         new_kmod_dir = origin_kmod_loc.parent / "custom_module_location"
@@ -32,7 +36,7 @@ def test_inhibit_if_custom_module_loaded(insert_custom_kmod, convert2rhel):
     """
     insert_custom_kmod()
     with convert2rhel(
-        ("-y --no-rpm-va --serverurl {} --username {} --password {} --pool {} --debug").format(
+        "-y --no-rpm-va --serverurl {} --username {} --password {} --pool {} --debug".format(
             env.str("RHSM_SERVER_URL"),
             env.str("RHSM_USERNAME"),
             env.str("RHSM_PASSWORD"),
@@ -59,7 +63,7 @@ def test_do_not_inhibit_if_module_is_not_loaded(shell, convert2rhel):
         prompt_amount = 3
     # If custom module is not loaded the conversion is not inhibited.
     with convert2rhel(
-        ("--no-rpm-va --serverurl {} --username {} --password {} --pool {} --debug").format(
+        "--no-rpm-va --serverurl {} --username {} --password {} --pool {} --debug".format(
             env.str("RHSM_SERVER_URL"),
             env.str("RHSM_USERNAME"),
             env.str("RHSM_PASSWORD"),
@@ -71,7 +75,9 @@ def test_do_not_inhibit_if_module_is_not_loaded(shell, convert2rhel):
             c2r.sendline("y")
             prompt_amount -= 1
 
-        assert c2r.expect("Kernel modules are compatible.", timeout=600) == 0
+        # Stop conversion before the point of no return as we do not need to run the full conversion
+        # with the check for kompatible kernel modules being right before the PONR.
+        assert c2r.expect("All loaded kernel modules are available in RHEL.", timeout=600) == 0
         c2r.expect("Continue with the system conversion?")
         c2r.sendline("n")
         assert c2r.exitstatus != 0
@@ -83,15 +89,16 @@ def test_do_not_inhibit_if_module_is_force_loaded(shell, convert2rhel):
     With the check for tainted kernel modules being at the beginning of the script, abort the conversion ASAP
     for the sake of test speed.
     """
+    # Force load the kernel module
     assert shell("modprobe -f -v bonding").returncode == 0
-    # Check for force loaded modules being in /proc/modules
+    # Check for force loaded modules being flagged FE in /proc/modules
     assert "(FE)" in shell("cat /proc/modules").output
 
     with convert2rhel("--no-rpm-va --debug") as c2r:
         assert c2r.expect("Tainted kernel module\(s\) detected") == 0
         assert c2r.exitstatus != 0
 
-    # Clean up
+    # Clean up - unload kmod and check for force loaded modules not being in /proc/modules
     assert shell("modprobe -r -v bonding").returncode == 0
     assert "(FE)" not in shell("cat /proc/modules").output
 
@@ -99,9 +106,12 @@ def test_do_not_inhibit_if_module_is_force_loaded(shell, convert2rhel):
 def test_tainted_kernel_inhibitor(shell, convert2rhel):
     """
     This test marks the kernel as tainted which is not supported by Convert2RHEL.
+    We need to install specific kernel packages to build own custom kernel module.
+    Build own kmod from source file that has been copied to the testing machine during preparation phase.
+    This kmod marks the system with the P, O and E flags.
     """
 
-    # We need to install specific kernel packages to build own custom kernel module.
+    # Install kernel packages
     shell("yum -y install gcc make kernel-headers kernel-devel-$(uname -r) elfutils-libelf-devel")
 
     # Build own kmod form source file that has been copied to the testing machine during preparation phase.
@@ -110,12 +120,12 @@ def test_tainted_kernel_inhibitor(shell, convert2rhel):
     assert shell("insmod /tmp/my-test/my_kmod.ko").returncode == 0
 
     with convert2rhel(
-        ("-y --no-rpm-va --serverurl {} --username {} --password {} --pool {} --debug").format(
+        "-y --no-rpm-va --serverurl {} --username {} --password {} --pool {} --debug".format(
             env.str("RHSM_SERVER_URL"),
             env.str("RHSM_USERNAME"),
             env.str("RHSM_PASSWORD"),
             env.str("RHSM_POOL"),
         )
     ) as c2r:
-        c2r.expect("Tainted kernel module\(s\) detected")
+        c2r.expect("Tainted kernel modules detected")
     assert c2r.exitstatus != 0
