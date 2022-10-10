169979df2bc4e787e6f843f340ea3aac847a4b3a
Daniel Diblik <ddiblik@redhat.com>
2022-10-10 11:13:53 +0200

    Add tier1 sanity test
    * add a test for basic conversion just to verify sanity
    
    Signed-off-by: Daniel Diblik <ddiblik@redhat.com>

---
+++ tests/integration/tier1/sanity/test_sanity_conversion.py
@@ -0,0 +1,20 @@
+from envparse import env
+
+
+def test_sanity_conversion(convert2rhel, shell):
+    """
+    Sanity only.
+    Verify successful full conversion.
+    No preparation or adjustments.
+    """
+
+    with convert2rhel(
+        ("-y --no-rpm-va --serverurl {} --username {} --password {} --pool {} --debug").format(
+            env.str("RHSM_SERVER_URL"),
+            env.str("RHSM_USERNAME"),
+            env.str("RHSM_PASSWORD"),
+            env.str("RHSM_POOL"),
+        )
+    ) as c2r:
+        c2r.expect("Conversion successful!")
+    assert c2r.exitstatus == 0
