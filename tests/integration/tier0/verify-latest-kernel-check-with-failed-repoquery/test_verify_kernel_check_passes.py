ea1df87c12efb59e667524dc9f8c857e583f360f
Daniel Diblik <ddiblik@redhat.com>
2022-10-17 17:56:55 +0200

    Incorporate review suggestions
    define the broken repofile as a file to copy to the target
    remove debuging remnant
    add condition for EUS CentOS, for different reposdir
    
    Signed-off-by: Daniel Diblik <ddiblik@redhat.com>

--- tests/integration/tier0/verify-latest-kernel-check-with-failed-repoquery/test_verify_kernel_check_passes.py
+++ tests/integration/tier0/verify-latest-kernel-check-with-failed-repoquery/test_verify_kernel_check_passes.py
@@ -1,3 +1,5 @@
+import platform
+
 import pytest
 
 
@@ -8,15 +10,16 @@ def test_verify_latest_kernel_check_passes_with_failed_repoquery(shell, convert2
     Failed repoquery subsequently caused the latest kernel check to fail.
     Introduced fixes should get the process past the latest kernel check.
     """
-
-    repopath = "/etc/yum.repos.d/tainted_repo.repo"
-    tainted_repo_setup = "[tainted-repo]\nname=Tainted repository\nbaseurl=http://download.eng.bos.redhat.com/beakerrepos/client/Fedor36/\nenabled=1\ngpgcheck=0"
+    get_system_release = platform.platform()
+    repofile = "broken_repo"
 
     # Add a tainted repository with intentionally bad baseurl so the repoquery fails successfully.
-    with open(repopath, "w") as tainted_repo:
-        tainted_repo.write(tainted_repo_setup)
-
-    assert shell(f"cat {repopath}").output == tainted_repo_setup
+    # For CentOS EUS we are working with hardcoded repos in /usr/share/convert2rhel/repos/
+    # We are skipping (at the test metadata level) the test for Oracle Linux EUS version,
+    # as the repositories are not available at all
+    if "centos-8.4" in get_system_release:
+        shell(f"cp -r files/{repofile}.repo /usr/share/convert2rhel/repos/")
+    shell(f"cp -r files/{repofile}.repo /etc/yum.repos.d/")
 
     # Run the conversion just past the latest kernel check, if successful, end the conversion there.
     with convert2rhel("--debug --no-rpm-va") as c2r:
@@ -34,4 +37,6 @@ def test_verify_latest_kernel_check_passes_with_failed_repoquery(shell, convert2
     assert c2r.exitstatus != 0
 
     # Cleanup the tainted repository.
-    assert shell(f"rm -f {repopath}").returncode == 0
+    if "centos-8.4" in get_system_release:
+        assert shell(f"rm -f /usr/share/convert2rhel/repos/{repofile}.repo") == 0
+    assert shell(f"rm -f /etc/yum.repos.d/{repofile}.repo").returncode == 0
