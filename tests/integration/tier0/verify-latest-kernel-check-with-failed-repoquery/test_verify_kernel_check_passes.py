cf11716a6a8815688f86d8c445a11864996fc069
Daniel Diblik <ddiblik@redhat.com>
2022-11-03 14:10:58 +0100

    Debug CentOS
    * correct the custom reposdir for centos
    * move adjustment to plan
    
    Signed-off-by: Daniel Diblik <ddiblik@redhat.com>

--- tests/integration/tier0/verify-latest-kernel-check-with-failed-repoquery/test_verify_kernel_check_passes.py
+++ tests/integration/tier0/verify-latest-kernel-check-with-failed-repoquery/test_verify_kernel_check_passes.py
@@ -12,13 +12,19 @@ def test_verify_latest_kernel_check_passes_with_failed_repoquery(shell, convert2
     """
     get_system_release = platform.platform()
     repofile = "broken_repo"
+    centos_custom_reposdir = "/usr/share/convert2rhel/repos/"
 
     # Add a tainted repository with intentionally bad baseurl so the repoquery fails successfully.
-    # For CentOS EUS we are working with hardcoded repos in /usr/share/convert2rhel/repos/
+    # For CentOS we are working with hardcoded repos in /usr/share/convert2rhel/repos/centos-8.{4,5}
     # We are skipping (at the test metadata level) the test for Oracle Linux EUS version,
     # as the repositories are not available at all
+
+    # TODO after the #619 gets merged, squash condition to centos-8 only
+    # TODO and copy to {centos_custom_reposdir}/{get_system_release}/
     if "centos-8.4" in get_system_release:
-        shell(f"cp -r files/{repofile}.repo /usr/share/convert2rhel/repos/")
+        shell(f"cp -r files/{repofile}.repo {centos_custom_reposdir}/centos-8.4/")
+    elif "centos-8.5" in get_system_release:
+        shell(f"cp -r files/{repofile}.repo {centos_custom_reposdir}/centos-8.5/")
     shell(f"cp -r files/{repofile}.repo /etc/yum.repos.d/")
 
     # Run the conversion just past the latest kernel check, if successful, end the conversion there.
@@ -38,5 +44,7 @@ def test_verify_latest_kernel_check_passes_with_failed_repoquery(shell, convert2
 
     # Cleanup the tainted repository.
     if "centos-8.4" in get_system_release:
-        assert shell(f"rm -f /usr/share/convert2rhel/repos/{repofile}.repo") == 0
+        assert shell(f"rm -f {centos_custom_reposdir}/centos-8.4/{repofile}.repo").returncode == 0
+    if "centos-8.5" in get_system_release:
+        assert shell(f"rm -f {centos_custom_reposdir}/centos-8.5/{repofile}.repo").returncode == 0
     assert shell(f"rm -f /etc/yum.repos.d/{repofile}.repo").returncode == 0
