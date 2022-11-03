eafdedf75311ce8b3c6c3699e038246e92fc628b
Rodolfo Olivieri <rodolfo.olivieri3@gmail.com>
2022-11-03 11:41:09 -0300

    Apply suggestions from code review
    Co-authored-by: Daniel Diblik <8378124+danmyway@users.noreply.github.com>

--- tests/integration/tier0/custom-kernel/test_cutsom_kernel.py
+++ tests/integration/tier0/custom-kernel/test_cutsom_kernel.py
@@ -7,14 +7,19 @@ def test_custom_kernel(convert2rhel):
     Run the conversion with custom kernel installed on the system.
 
     """
-    system_version = platform.platform()
+    get_system_release = platform.platform()
 
-    if "centos" in system_version:
+    if "centos" in get_system_release:
         string = "CentOS"
-    elif "oracle" in system_version:
+    elif "oracle" in get_system_release:
         string = "Oracle"
+    if os.environ["TMT_REBOOT_COUNT"] == "0":
+        install_custom_kernel(shell)
+    elif os.environ["TMT_REBOOT_COUNT"] == "1":
+        with convert2rhel(("--no-rpm-va --debug")) as c2r:
+            c2r.expect("WARNING - Custom kernel detected. The booted kernel needs to be signed by {}".format(string))
+            c2r.expect("CRITICAL - The booted kernel version is incompatible with the standard RHEL kernel.")
+        assert c2r.exitstatus != 0
 
-    with convert2rhel(("--no-rpm-va --debug")) as c2r:
-        c2r.expect("WARNING - Custom kernel detected. The booted kernel needs to be signed by {}".format(string))
-        c2r.expect("CRITICAL - The booted kernel version is incompatible with the standard RHEL kernel.")
-    assert c2r.exitstatus != 0
+        # Restore the system.
+        clean_up_custom_kernel(shell)
