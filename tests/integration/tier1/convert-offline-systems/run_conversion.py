a8b208f2c46086f762c97cb5433d504bf8dafb8e
kokesak <43987098+kokesak@users.noreply.github.com>
2022-11-08 12:49:26 +0100

    [RHELC-732] Change satellite URL (#652)
    * Change satellite URL
    
    * Replace non-eus satellite test with rhsm equivalent
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- tests/integration/tier1/convert-offline-systems/run_conversion.py
+++ tests/integration/tier1/convert-offline-systems/run_conversion.py
@@ -1,5 +1,4 @@
 import os
-import platform
 
 from envparse import env
 
@@ -9,23 +8,12 @@ def test_convert_offline_systems(convert2rhel):
 
     os.environ["CONVERT2RHEL_UNSUPPORTED_INCOMPLETE_ROLLBACK"] = "1"
     os.environ["CONVERT2RHEL_UNSUPPORTED_SKIP_KERNEL_CURRENCY_CHECK"] = "1"
-    source_distro = platform.platform()
 
-    if "centos-8.4" in source_distro or "oracle-8.4" in source_distro:
-        with convert2rhel(
-            ("-y --no-rpm-va -k {} -o {} --keep-rhsm --debug").format(
-                env.str("SATELLITE_KEY_EUS"),
-                env.str("SATELLITE_ORG"),
-            )
-        ) as c2r:
-            pass
-        assert c2r.exitstatus == 0
-    else:
-        with convert2rhel(
-            ("-y --no-rpm-va -k {} -o {} --keep-rhsm --debug").format(
-                env.str("SATELLITE_KEY"),
-                env.str("SATELLITE_ORG"),
-            )
-        ) as c2r:
-            pass
-        assert c2r.exitstatus == 0
+    with convert2rhel(
+        ("-y --no-rpm-va -k {} -o {} --keep-rhsm --debug").format(
+            env.str("SATELLITE_KEY"),
+            env.str("SATELLITE_ORG"),
+        )
+    ) as c2r:
+        pass
+    assert c2r.exitstatus == 0
