562984e595c3675d7e7ab93e331e221f8a83b0ab
Martin Litwora <mlitwora@redhat.com>
2022-11-11 01:06:49 +0100

    Try to fix the test

--- tests/integration/tier0/custom-kernel/test_custom_kernel.py
+++ tests/integration/tier0/custom-kernel/test_custom_kernel.py
@@ -7,29 +7,35 @@ SYSTEM_RELEASE = os.environ.get("SYSTEM_RELEASE")
 
 DISTRO_KERNEL_MAPPING = {
     "centos-7": {
-        "original_kernel": "kernel-3.10.0-1160.76.1.el7.x86_64",
+        "original_kernel": "kernel-3.10.0-1160.80.1.el7.x86_64",
         "custom_kernel": "https://yum.oracle.com/repo/OracleLinux/OL7/latest/x86_64/getPackage/kernel-3.10.0-1160.76.1.0.1.el7.x86_64.rpm",
+        "grub_substring": "CentOS Linux (3.10.0-1160.76.1.0.1.el7.x86_64) 7 (Core)"
     },
     "centos-8.4": {
         "original_kernel": "kernel-core-4.18.0-305.25.1.el8_4.x86_64",
         "custom_kernel": "https://yum.oracle.com/repo/OracleLinux/OL8/4/baseos/base/x86_64/getPackage/kernel-core-4.18.0-305.el8.x86_64.rpm",
+        "grub_substring": "Oracle Linux Server (4.18.0-305.el8.x86_64) 8.4"
     },
     "centos-8.5": {
         "original_kernel": "kernel-core-4.18.0-348.7.1.el8_5.x86_64",
         "custom_kernel": "https://yum.oracle.com/repo/OracleLinux/OL8/5/baseos/base/x86_64/getPackage/kernel-core-4.18.0-348.el8.x86_64.rpm",
+        "grub_substring": "Oracle Linux Server (4.18.0-348.el8.x86_64) 8.5"
     },
     "oracle-7": {
-        "original_kernel": "kernel-uek-5.4.17-2136.307.3.1.el7uek.x86_64",
+        "original_kernel": "kernel-3.10.0-1160.80.1.0.1.el7.x86_64",
         "custom_kernel": "http://mirror.centos.org/centos/7/os/x86_64/Packages/kernel-3.10.0-1160.el7.x86_64.rpm",
+        "grub_substring": "Oracle Linux Server 7.9, with Linux 3.10.0-1160.el7.x86_64"
     },
     "oracle-8.4": {
         "original_kernel": "kernel-uek-5.4.17-2102.204.4.4.el8uek.x86_64",
         "custom_kernel": " https://vault.centos.org/centos/8.4.2105/BaseOS/x86_64/os/Packages/kernel-core-4.18.0-305.25.1.el8_4.x86_64.rpm",
+        "grub_substring": "CentOS Linux (4.18.0-305.25.1.el8_4.x86_64) 8"
     },
     # Install CentOS 8.5 kernel
     "oracle-8.6": {
         "original_kernel": "kernel-uek-5.4.17-2136.307.3.1.el8uek.x86_64",
         "custom_kernel": "https://vault.centos.org/centos/8.5.2111/BaseOS/x86_64/os/Packages/kernel-core-4.18.0-348.7.1.el8_5.x86_64.rpm",
+        "grub_substring": "CentOS Linux (4.18.0-348.7.1.el8_5.x86_64) 8"
     },
 }
 
@@ -39,26 +45,35 @@ def install_custom_kernel(shell):
     Install CentOS kernel on Oracle Linux and vice versa to mimic the custom
     kernel that is not signed by the running OS official vendor.
     """
-    _, custom_kernel = DISTRO_KERNEL_MAPPING[SYSTEM_RELEASE].values()
-    kernel_release = custom_kernel.rsplit("/")[-1].replace(".rpm", "").split("-", 2)[-1]
-    if "centos-8.5" in SYSTEM_RELEASE:
+
+    _, custom_kernel, grub_substring = DISTRO_KERNEL_MAPPING[SYSTEM_RELEASE].values()
+    #kernel_release = custom_kernel.rsplit("/")[-1].replace(".rpm", "").split("-", 2)[-1]
+    #if "centos-8.5" in SYSTEM_RELEASE:
         # Reinstall the kernel custom kernel replacing the one that is installed
         # on CentOS 8.5 that has the same version.
-        assert shell("dnf reinstall  %s -y" % custom_kernel) == 0
-    else:
-        assert shell("yum install %s -y" % custom_kernel).returncode == 0
+        #assert shell("dnf reinstall  %s -y" % custom_kernel) == 0
+    #else:
+        #The reinstall did not work as it said that package is not installed thus cannot uninstall it
+        # We have to remove this kernel-core package first, as the ones we try
+        # to install from Oracle Linux are the same version.
+        # weird..
+        #assert shell("yum remove %s -y" % original_kernel) == 0
 
-    assert (
-        shell(
-            "grubby --set-default=/boot/vmlinuz-%s" % kernel_release,
-        ).returncode
-        == 0
-    )
+    assert shell("yum install %s -y" % custom_kernel).returncode == 0
+
+    assert shell("grub2-set-default '%s'" % grub_substring).returncode == 0
+
+    # assert (
+    #     shell(
+    #         "grubby --set-default=/boot/vmlinuz-%s" % kernel_release,
+    #     ).returncode
+    #     == 0
+    # )
 
     shell("tmt-reboot -t 600")
 
 
-def clean_up_custom_kernel(shell):
+def clean_up_custom_kernel(shell, original_kernel_uname):
     """
     Remove the current installed kernel and install the machine default kernel.
     """
@@ -67,13 +82,14 @@ def clean_up_custom_kernel(shell):
     custom_kernel_release = custom_kernel.rsplit("/")[-1].replace(".rpm", "")
     assert shell("rpm -e %s" % custom_kernel_release).returncode == 0
 
+
     # Install back the CentOS 8.5 original kernel
     if "centos-8.5" in SYSTEM_RELEASE:
         assert shell("yum install %s -y" % original_kernel).returncode == 0
 
     assert (
         shell(
-            "grubby --set-default=/boot/vmlinuz-%s" % original_kernel_release,
+            "grubby --set-default=/boot/vmlinuz-%s" % original_kernel_uname,
         ).returncode
         == 0
     )
@@ -98,6 +114,8 @@ def test_custom_kernel(convert2rhel, shell):
         os_vendor = "Oracle"
 
     if os.environ["TMT_REBOOT_COUNT"] == "0":
+        original_kernel = shell("uname -r").output.strip()
+        print(original_kernel)
         install_custom_kernel(shell)
     elif os.environ["TMT_REBOOT_COUNT"] == "1":
         with convert2rhel("--no-rpm-va --debug") as c2r:
@@ -106,5 +124,5 @@ def test_custom_kernel(convert2rhel, shell):
         assert c2r.exitstatus != 0
 
         # Restore the system.
-        clean_up_custom_kernel(shell)
+        clean_up_custom_kernel(shell, original_kernel)
         shell("tmt-reboot -t 600")
