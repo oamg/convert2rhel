32a175b0f12f385f8d40e306ed610752dcb856dc
Daniel Diblik <8378124+danmyway@users.noreply.github.com>
2022-11-22 11:50:13 +0100

    Re-enable testing on Oracle Linux 8.6 (#667)
    * Re-enable testing on Oracle Linux 8.6
    
    * OL8.7 is officially out, re-enable 8.6 as a target to run tests on
    * the target will get updated using ansible to 8.7 latest
    * when 8.7 image is available on testing farm, switch target to 8.7
    
    Signed-off-by: Daniel Diblik <ddiblik@redhat.com>
    
    * Tweak custom_kernel test and SYSTEM_RELEASE envar
    
    * all the distributions apart from Oracle Linux 8.7 can have the
      original kernel hardcoded, as it is not likely to change
    * assign a variable for original kernel for OL8.7
    * polish clean up phase
    * assign oracle-8.7 envar to oraclelinux-8.6 target until the ol 8.7
      image is available at testing farm
    
    Signed-off-by: Daniel Diblik <ddiblik@redhat.com>
    
    * Apply suggestions
    
    * original_kernel to variable for all except centos8
    * comment .packit.yaml
    
    Signed-off-by: Daniel Diblik <ddiblik@redhat.com>
    
    Signed-off-by: Daniel Diblik <ddiblik@redhat.com>

--- tests/integration/tier0/custom-kernel/test_custom_kernel.py
+++ tests/integration/tier0/custom-kernel/test_custom_kernel.py
@@ -5,14 +5,17 @@ import pytest
 
 SYSTEM_RELEASE = os.environ.get("SYSTEM_RELEASE")
 
+ORIGINAL_KERNEL = os.popen("rpm -q --last kernel | head -1 | cut -d ' ' -f1").read()
+
 DISTRO_KERNEL_MAPPING = {
     "centos-7": {
-        "original_kernel": "kernel-3.10.0-1160.80.1.el7.x86_64",
+        "original_kernel": f"{ORIGINAL_KERNEL}",
         "custom_kernel": "https://yum.oracle.com/repo/OracleLinux/OL7/latest/x86_64/getPackage/kernel-3.10.0-1160.76.1.0.1.el7.x86_64.rpm",
         "grub_substring": "CentOS Linux (3.10.0-1160.76.1.0.1.el7.x86_64) 7 (Core)",
     },
+    # We hardcode original kernel for both CentOS 8.4 and CentOS 8.5 as it won't receive any updates anymore
     "centos-8.4": {
-        "original_kernel": "kernel-core-4.18.0-305.25.1.el8_4.x86_64",
+        "original_kernel": f"kernel-core-4.18.0-305.25.1.el8_4.x86_64",
         "custom_kernel": "https://yum.oracle.com/repo/OracleLinux/OL8/4/baseos/base/x86_64/getPackage/kernel-core-4.18.0-305.el8.x86_64.rpm",
         "grub_substring": "Oracle Linux Server (4.18.0-305.el8.x86_64) 8.4",
     },
@@ -22,18 +25,13 @@ DISTRO_KERNEL_MAPPING = {
         "grub_substring": "Oracle Linux Server (4.18.0-348.el8.x86_64) 8.5",
     },
     "oracle-7": {
-        "original_kernel": "kernel-3.10.0-1160.80.1.0.1.el7.x86_64",
+        "original_kernel": f"{ORIGINAL_KERNEL}",
         "custom_kernel": "http://mirror.centos.org/centos/7/os/x86_64/Packages/kernel-3.10.0-1160.el7.x86_64.rpm",
         "grub_substring": "Oracle Linux Server 7.9, with Linux 3.10.0-1160.el7.x86_64",
     },
-    "oracle-8.4": {
-        "original_kernel": "kernel-core-4.18.0-305.el8.x86_64",
-        "custom_kernel": " https://vault.centos.org/centos/8.4.2105/BaseOS/x86_64/os/Packages/kernel-core-4.18.0-305.25.1.el8_4.x86_64.rpm",
-        "grub_substring": "CentOS Linux (4.18.0-305.25.1.el8_4.x86_64) 8",
-    },
     # Install CentOS 8.5 kernel
-    "oracle-8.6": {
-        "original_kernel": "kernel-core-4.18.0-372.32.1.0.1.el8_6.x86_64",
+    "oracle-8.7": {
+        "original_kernel": f"{ORIGINAL_KERNEL}",
         "custom_kernel": "https://vault.centos.org/centos/8.5.2111/BaseOS/x86_64/os/Packages/kernel-core-4.18.0-348.7.1.el8_5.x86_64.rpm",
         "grub_substring": "CentOS Linux (4.18.0-348.7.1.el8_5.x86_64) 8",
     },
@@ -70,7 +68,7 @@ def clean_up_custom_kernel(shell):
 
     assert (
         shell(
-            "grubby --set-default /boot/vmlinuz-%s" % original_kernel_release,
+            "grubby --set-default /boot/vmlinuz-*%s" % original_kernel_release,
         ).returncode
         == 0
     )
