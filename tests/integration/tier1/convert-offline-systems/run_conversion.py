e683f8583932ba097f7e12642d3b31a8e24bbb35
Martin Litwora <mlitwora@redhat.com>
2022-11-24 17:30:46 +0100

    Try different workaround for cent7

--- tests/integration/tier1/convert-offline-systems/run_conversion.py
+++ tests/integration/tier1/convert-offline-systems/run_conversion.py
@@ -6,9 +6,6 @@ from envparse import env
 def test_convert_offline_systems(convert2rhel):
     """Test converting systems not connected to the Internet but requiring sub-mgr (e.g. managed by Satellite)."""
 
-    # TODO: Remove once the repos of CentOS7 are synced with the latest packages
-    os.environ["CONVERT2RHEL_UNSUPPORTED_SKIP_KERNEL_CURRENCY_CHECK"] = "1"
-
     with convert2rhel(
         ("-y --no-rpm-va -k {} -o {} --keep-rhsm --debug").format(
             env.str("SATELLITE_KEY"),
