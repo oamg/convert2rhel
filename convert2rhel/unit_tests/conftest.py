881af68f9585d1f3e8858844cd5dea2b3ce9ae00
Rodolfo Olivieri <rolivier@redhat.com>
2023-01-11 11:45:56 -0300

    [RHELC-858] Remove configs for RHEL6 (#699)

--- convert2rhel/unit_tests/conftest.py
+++ convert2rhel/unit_tests/conftest.py
@@ -288,18 +288,11 @@ def pretend_os(request, pkg_root, monkeypatch):
 all_systems = pytest.mark.parametrize(
     "pretend_os",
     (
-        ("6.10.1111", "CentOS Linux"),
-        ("6.10.1111", "Oracle Linux Server"),
         ("7.9.1111", "CentOS Linux"),
         ("7.9.1111", "Oracle Linux Server"),
         ("8.4.1111", "CentOS Linux"),
         ("8.4.1111", "Oracle Linux Server"),
     ),
-    indirect=True,
-)
-centos6 = pytest.mark.parametrize(
-    "pretend_os",
-    (("6.10.1111", "CentOS Linux"),),
     indirect=True,
 )
 centos7 = pytest.mark.parametrize(
