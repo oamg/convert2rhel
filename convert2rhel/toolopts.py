4e604464ed7010b4e4f1255d7fb857da55593bb8
Adam Hosek <hosek.adam@outlook.com>
2023-02-07 13:28:00 +0100

    [RHELC-693] Check if org and key passed (#563)

--- convert2rhel/toolopts.py
+++ convert2rhel/toolopts.py
@@ -420,6 +420,11 @@ class CLI(object):
                 " We're going to use the password file."
             )
 
+        if (tool_opts.org and not tool_opts.activation_key) or (not tool_opts.org and tool_opts.activation_key):
+            loggerinst.critical(
+                "Either the --organization or the --activationkey option is missing. You can't use one without the other."
+            )
+
         if tool_opts.username and tool_opts.password:
             tool_opts.credentials_thru_cli = True
 
