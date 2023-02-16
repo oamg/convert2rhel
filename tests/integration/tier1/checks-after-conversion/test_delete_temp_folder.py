52671a480b4b14633d296dd096f25703ff800817
Daniel Diblik <ddiblik@redhat.com>
2023-02-16 15:06:04 +0100

    Make an after conversion check
    * instead of having a full conversion running just to check for
      initramfs and vmlinuz files presence, introduce a check after each
    conversion
    * scrap the individual test case
    * deselect said check from cases with tainted files
    
    Signed-off-by: Daniel Diblik <ddiblik@redhat.com>

--- tests/integration/tier1/checks-after-conversion/test_delete_temp_folder.py
+++ tests/integration/tier1/checks-after-conversion/test_delete_temp_folder.py
@@ -1,7 +1,10 @@
 import os
 
+import pytest
 
+
+@pytest.mark.delete_temporary_folder
 def test_temp_folder():
-    """Testing, if the temporary folder was sucessfully removed after conversion"""
+    """Testing, if the temporary folder was successfully removed after conversion"""
     tmp_folder = "/var/lib/convert2rhel/"
     assert not os.path.exists(tmp_folder)
