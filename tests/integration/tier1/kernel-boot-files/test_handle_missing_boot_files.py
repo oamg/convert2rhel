068e4292186dfa2548b62512d41d3d19ff8460d7
Michal Bocek <mbocek@redhat.com>
2023-02-22 16:16:04 +0100

    Fix "missing boot files" test (#748)
    On CentOS and Oracle Linux 7 the test was failing because we were
    renaming the vmlinuz and initramfs files in order to back them up, but
    we left them in the /boot/ folder. That caused the grub2-mkconfig to
    pick them up and generated a new grub configuration using these renamed
    boot files.
    
    This change does not back up the boot files but instead simply removes
    them, simulating better the core test scenario of this integration test
    which is the kernel rpm scriptlet failure to copy/generate these boot
    files. After verifying that convert2rhel detected this situation of
    missing boot files correctly, the test goes on to do what the user
    instructed to do in such a case - that is reinstalling the kernel and
    re-runing grub2-mkconfig.
    
    Fixes https://github.com/oamg/convert2rhel/pull/721.

--- tests/integration/tier1/kernel-boot-files/test_handle_missing_boot_files.py
+++ tests/integration/tier1/kernel-boot-files/test_handle_missing_boot_files.py
@@ -10,9 +10,6 @@ from envparse import env
 INITRAMFS_FILE = "/boot/initramfs-%s.img"
 VMLINUZ_FILE = "/boot/vmlinuz-%s"
 
-BACKUP_INITRAMFS_FILE = "/boot/initramfs-%s.backup.img"
-BACKUP_VMLINUZ_FILE = "/boot/vmlinuz-%s.backup"
-
 
 def get_latest_installed_kernel_version(kernel_name):
     """Utility function to get the latest installed kernel."""
@@ -23,47 +20,19 @@ def get_latest_installed_kernel_version(kernel_name):
     return latest_installed_kernel.strip()
 
 
-def restore_original_kernel_boot_files(shell, kernel_version):
-    """
-    Utility function to restore the required kernel boot files after the
-    conversion.
-    """
+def remove_kernel_boot_files(shell, kernel_version):
+    """Utility function to remove the RHEL kernel boot files."""
+
     initramfs_file = INITRAMFS_FILE % kernel_version
     vmlinuz_file = VMLINUZ_FILE % kernel_version
 
-    # Assert that the original files still doesn't exists
-    assert not os.path.exists(initramfs_file)
-    assert not os.path.exists(vmlinuz_file)
-
-    initramfs_backup = BACKUP_INITRAMFS_FILE % kernel_version
-    vmlinuz_backup = BACKUP_VMLINUZ_FILE % kernel_version
-
-    # Move the "backup" files back with the correct name
-    assert shell("mv %s %s" % (initramfs_backup, initramfs_file)).returncode == 0
-    assert shell("mv %s %s" % (vmlinuz_backup, vmlinuz_file)).returncode == 0
-
-    # Assert that both files exists now
+    # Assert that we have the files in the /boot/ folder
     assert os.path.exists(initramfs_file)
     assert os.path.exists(vmlinuz_file)
 
-
-def backup_kernel_boot_files(shell, kernel_version):
-    """
-    Utility function to backup the required kernel boot files.
-    """
-    initramfs_file = INITRAMFS_FILE % kernel_version
-    vmlinuz_file = VMLINUZ_FILE % kernel_version
-
-    # Assert that we have the files in the folder
-    assert os.path.exists(initramfs_file)
-    assert os.path.exists(vmlinuz_file)
-
-    initramfs_backup = BACKUP_INITRAMFS_FILE % kernel_version
-    vmlinuz_backup = BACKUP_VMLINUZ_FILE % kernel_version
-
-    # Move the original files to be a "backup", simulating that we deleted them
-    assert shell("mv %s %s" % (initramfs_file, initramfs_backup)).returncode == 0
-    assert shell("mv %s %s" % (vmlinuz_file, vmlinuz_backup)).returncode == 0
+    # Remove the installed RHEL kernel boot files, simulating that they failed to be generated during the conversion
+    assert shell("rm -f %s" % initramfs_file).returncode == 0
+    assert shell("rm -f %s" % vmlinuz_file).returncode == 0
 
 
 @pytest.mark.missing_kernel_boot_files
@@ -77,8 +46,8 @@ def test_missing_kernel_boot_files(convert2rhel, shell):
     the scriptlet can fail, the transaction will still continue and the
     workflow will continue to be executed. The problem is that, with a
     scriptlet failure when replacing/installing a kernel, the initramfs and
-    vmlinuz could not be available in the /boot partition, especially if there
-    is no sufficient disk space available in there. This test has the intention
+    vmlinuz may not be available in the /boot partition, especially if there
+    isn't sufficient disk space available in there. This test has the intention
     to verify that the warning with the correct steps are provided to the
     user in order to overcome this case and fix it for them.
     """
@@ -100,17 +69,19 @@ def test_missing_kernel_boot_files(convert2rhel, shell):
         kernel_version = get_latest_installed_kernel_version(kernel_name)
 
         # We want to simulate with this test that when we don't detect the
-        # initramfs and vmlinuz files, we will produce an warning and tell the
+        # initramfs and vmlinuz files, we will produce a warning and tell the
         # user what to do in order to fix the problem. To not cause any more
         # mess other than what we want, let's just remove the two file from the
         # system, and see if Convert2RHEL will detect that the right way.
-        backup_kernel_boot_files(shell, kernel_version)
+        remove_kernel_boot_files(shell, kernel_version)
 
         assert c2r.expect("Couldn't verify the kernel boot files in the boot partition.") == 0
 
-        # We have to restore the original initramfs file in order to use the
-        # checks-after-conversion tests to assert that most of the conversion
-        # is done properly.
-        restore_original_kernel_boot_files(shell, kernel_version)
+        # We have to restore the boot files in order to use the checks-after-conversion tests to
+        # assert that the rest of the conversion has succeeded.
+        # We'll do that the same way we're telling the user in a warning message how to fix the problem.
+        # That is by reinstalling the RHEL kernel and re-running grub2-mkconfig.
+        assert shell("yum reinstall kernel-{} -y".format(kernel_version)).returncode == 0
+        assert shell("grub2-mkconfig -o /boot/grub2/grub.cfg").returncode == 0
 
     assert c2r.exitstatus == 0
