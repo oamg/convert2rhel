635027850e06cd016f2ac675ec1ef16c169bf6d1
Toshio Kuratomi <tkuratom@redhat.com>
2023-03-15 12:50:22 -0700

    Code to autodetect and load all the Actions.

--- convert2rhel/unit_tests/actions/actions_test.py
+++ convert2rhel/unit_tests/actions/actions_test.py
@@ -14,6 +14,8 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
+__metaclass__ = type
+
 import os.path
 import re
 import unittest
@@ -90,6 +92,65 @@ REPOQUERY_L_STUB_BAD = (
     "/lib/modules/5.8.0-7642-generic/kernel/lib/f.ko.xz\n"
     "/lib/modules/5.8.0-7642-generic/kernel/lib/f.ko\n"
 )
+
+
+class TestGetActions:
+    ACTION_CLASS_DEFINITION_RE = re.compile(r"^class .+\([^)]*Action\):$", re.MULTILINE)
+
+    def test_get_actions_smoketest(self):
+        """Test that there are no errors loading the Actions we ship."""
+        computed_actions = actions.get_actions(actions.__path__, actions.__name__ + ".")
+
+        # Is this method of finding how many Action plugins we ship too hacky?
+        filesystem_detected_actions_count = 0
+        for rootdir, dirnames, filenames in os.walk(os.path.dirname(actions.__file__)):
+            for filename in (os.path.join(rootdir, filename) for filename in filenames):
+                if filename.endswith(".py") and not filename.endswith("/__init__.py"):
+                    with open(filename) as f:
+                        action_classes = self.ACTION_CLASS_DEFINITION_RE.findall(f.read())
+                        filesystem_detected_actions_count += len(action_classes)
+
+        assert len(computed_actions) == filesystem_detected_actions_count
+
+    def test_no_actions(self, tmpdir):
+        """No Actions returns an empty list."""
+        # We need to make sure this returns an empty list, not just false-y
+        assert (
+            actions.get_actions([str(tmpdir)], "tmp.") == []  # pylint: disable=use-implicit-booleaness-not-comparison
+        )
+
+    @pytest.mark.parametrize(
+        (
+            "test_dir_name",
+            "expected_action_names",
+        ),
+        (
+            ("aliased_action_name", ["RealTest"]),
+            ("extraneous_files", ["RealTest"]),
+            ("ignore__init__", ["RealTest"]),
+            ("multiple_actions_one_file", ["RealTest", "SecondTest"]),
+            ("not_action_itself", ["RealTest", "OtherTest"]),
+            ("only_subclasses_of_action", ["RealTest"]),
+        ),
+    )
+    def test_found_actions(self, sys_path, test_dir_name, expected_action_names):
+        """Set of Actions that we have generated is found."""
+        data_dir = os.path.join(os.path.dirname(__file__), "data")
+        sys_path.insert(0, data_dir)
+        test_data = os.path.join(data_dir, test_dir_name)
+        computed_action_names = sorted(
+            m.__name__
+            for m in actions.get_actions([test_data], "convert2rhel.unit_tests.actions.data.%s." % test_dir_name)
+        )
+        assert computed_action_names == sorted(expected_action_names)
+
+
+class TestResolveActionOrder:
+    pass
+
+
+class TestRunActions:
+    pass
 
 
 def test_perform_pre_checks(monkeypatch):
