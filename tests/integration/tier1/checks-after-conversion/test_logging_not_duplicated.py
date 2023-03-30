51a5478a03f6397e61430e8fc745343ff27ab910
Daniel Diblik <8378124+danmyway@users.noreply.github.com>
2023-03-30 15:36:00 +0200

    Add a sanity check for logging duplication (#781)
    * Verify the lines in log file are unique and not duplicated
    * Interpret results as INFO until current duplication gets resolved
    
    Signed-off-by: Daniel Diblik <ddiblik@redhat.com>

---
+++ tests/integration/tier1/checks-after-conversion/test_logging_not_duplicated.py
@@ -0,0 +1,20 @@
+import pytest
+
+
+@pytest.mark.verify_logging_is_not_duplicated
+def test_verify_logging_is_not_duplicated():
+    """
+    Verify that the logfile does not contain duplicated lines.
+
+    This function goes through the log file line by line
+    and verifies the lines are unique and not duplicated.
+    """
+    log_file = "/var/log/convert2rhel/convert2rhel.log"
+    with open(log_file, "r") as log_file_data:
+        previous_line = None
+        for line in log_file_data:
+            line = line.strip()
+            if line != "":
+                assert line != previous_line
+
+            previous_line = line.strip()
