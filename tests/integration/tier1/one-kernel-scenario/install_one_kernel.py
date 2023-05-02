7fe99aeb022afb7f8317dd9a2bce65cd729cfe41
Daniel Diblik <ddiblik@redhat.com>
2023-05-02 15:40:49 +0200

    Fix pulp url
    * the repos were moved to https, address the changes
    
    Signed-off-by: Daniel Diblik <ddiblik@redhat.com>

--- tests/integration/tier1/one-kernel-scenario/install_one_kernel.py
+++ tests/integration/tier1/one-kernel-scenario/install_one_kernel.py
@@ -11,6 +11,6 @@ def test_install_one_kernel(shell):
         assert shell("grub2-set-default 'Oracle Linux Server 7.9, with Linux 3.10.0-1160.el7.x86_64'").returncode == 0
 
     # replace url in yum.repos.d rhel repo
-    original_url = "baseurl = http://rhsm-pulp.corp.redhat.com/content/dist/rhel/server/7/\$releasever/\$basearch/os/"
-    new_url = "baseurl=http://rhsm-pulp.corp.redhat.com/content/dist/rhel/server/7/7.9/x86_64/os/"
+    original_url = "baseurl = https://rhsm-pulp.corp.redhat.com/content/dist/rhel/server/7/\$releasever/\$basearch/os/"
+    new_url = "baseurl=https://rhsm-pulp.corp.redhat.com/content/dist/rhel/server/7/7.9/x86_64/os/"
     shell('sed -i "s+{}+{}+g" /etc/yum.repos.d/rhel7.repo'.format(original_url, new_url))
