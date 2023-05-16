8fc878f6c3340b1c74f870cfecb32edd72361576
Rodolfo Olivieri <rolivier@redhat.com>
2023-05-16 14:19:26 -0300

    [RHELC-1010] Handle empty arch in package information (#828)
    Co-authored-by: Daniel Diblik <ddiblik@redhat.com>

--- tests/integration/tier1/checks-after-conversion/test_verify_strings_in_log.py
+++ tests/integration/tier1/checks-after-conversion/test_verify_strings_in_log.py
@@ -1,3 +1,5 @@
+import re
+
 import pytest
 
 
@@ -12,3 +14,15 @@ def test_verify_initramfs_and_vmlinuz_present(log_file_data):
     and validating that the initramfs file is not corrupted.
     """
     assert "The initramfs and vmlinuz files are valid." in log_file_data
+
+
+@pytest.mark.test_failed_to_parse_package_info_empty_arch_not_present
+def test_failed_to_parse_a_package_not_present(log_file_data):
+    """
+    Verify that in case of package with the `arch` field missing in its information,
+    the message Failed to parse a package does not appear during the conversion run.
+    """
+
+    failed_to_parse = r"Failed to parse a package: Invalid package string - .+\.\(none\)"
+    match = re.search(failed_to_parse, log_file_data)
+    assert match is None, f"{failed_to_parse} is present in the log file data."
