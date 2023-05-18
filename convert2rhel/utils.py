053337eff14b73ec990a0f6ef746d85c77f07499
Toshio Kuratomi <tkuratom@redhat.com>
2023-05-18 01:24:07 -0700

    Do not honor CONVERT2RHEL_UNSUPPORTED_INCOMPLETE_ROLLBACK when in analyze mode.
    `analyze` depends on being able to rollback all changes to the system to get back to the initial
    state.  If INCOMPLETE_ROLLBACK is specified, we wouldn't be able to do that, which would leave the
    user with a system which can't be converted without manual intervention after running
    `convert2rhel analyze.`  So we need to prevent that from happening.
    
    Note that having the check for the environment variable and analyze activity in utils.download_pkg()
    isn't great.  It means that we will report a FAILURE when we get there whereas it would be more
    accurate to report a SKIP.  In the future we should redesign this so download_pkgs() raises an
    exception in this case and a higher level catches the exception and decides what to do based on the
    environment variable and the activity.

--- convert2rhel/utils.py
+++ convert2rhel/utils.py
@@ -717,14 +717,45 @@ def download_pkg(
     if ret_code != 0:
         loggerinst.warning("Output from the yumdownloader call:\n%s" % (output))
 
-        if not "CONVERT2RHEL_UNSUPPORTED_INCOMPLETE_ROLLBACK" in os.environ:
+        # Note: Checking toolopts here is a temporary solution. We need to
+        # restructure this to raise an exception on error and have the caller
+        # handle whether to use INCOMPLETE_ROLLBACK to do something for several
+        # reasons:
+        # (1) utils should be simple functions that take input and produce
+        #     output from it. Having knowledge of things specific to the
+        #     program (for instance, the environment variable that convert2rhel
+        #     uses) makes the utils depend on the specific place that they are
+        #     run instead.
+        # (2) Where an error condition arises, they should "return" that to the
+        #     caller to decide how to handle it by using an exception. Handling
+        #     it inside the function ties us to one specific behaviour on
+        #     error. (For instance, the incomplete rollback message here ties
+        #     downloading packages and performing rollbacks. But what about
+        #     downloading packages that are not tied to rollbacks. Maybe we
+        #     have to download a package in order for insights or
+        #     subscription-manager to run. In those cases, we either cannot use
+        #     this function or we might show the user a misleading message).
+        # (3) Functions in utils should be free of other dependencies within
+        #     convert2rhel.  That allows us to use utils with no fear of
+        #     circular dependency issues.
+        # (4) Making the choices here mean that when used inside of the Action
+        #     framework, we are limited to returning a FAILURE for the Action
+        #     plugin whereas returning SKIP would be more accurate.
+        from convert2rhel import toolopts
+
+        if (
+            "CONVERT2RHEL_UNSUPPORTED_INCOMPLETE_ROLLBACK" not in os.environ
+            or toolopts.tool_opts.activity != "conversion"
+        ):
             loggerinst.critical(
                 "Couldn't download the %s package. This means we will not be able to do a"
                 " complete rollback and may put the system in a broken state.\n"
                 "Check to make sure that the %s repositories are enabled"
                 " and the package is updated to its latest version.\n"
                 "If you would rather ignore this check set the environment variable"
-                " 'CONVERT2RHEL_UNSUPPORTED_INCOMPLETE_ROLLBACK'." % (pkg, system_info.name)
+                " 'CONVERT2RHEL_UNSUPPORTED_INCOMPLETE_ROLLBACK'. Note that the"
+                " environment variable is only consulted when actually converting a"
+                " system. It is disabled when performing a pre-conversion analysis." % (pkg, system_info.name)
             )
         else:
             loggerinst.warning(
