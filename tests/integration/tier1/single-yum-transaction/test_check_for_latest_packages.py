d80c7a885b0ed837014a8495d984853e39b0fb24
Daniel Diblik <ddiblik@redhat.com>
2023-05-25 15:38:53 +0200

    Override releasever for CentOS 8.5
    * we need to point the yum check-update to the correct minor
    
    Signed-off-by: Daniel Diblik <ddiblik@redhat.com>

--- tests/integration/tier1/single-yum-transaction/test_check_for_latest_packages.py
+++ tests/integration/tier1/single-yum-transaction/test_check_for_latest_packages.py
@@ -51,10 +51,12 @@ def test_packages_upgraded_after_conversion(convert2rhel, shell):
     # We need to point the releasever to 8.5 with CentOS latest
     # otherwise the yum check-update looks at releasever 8
     # discovering package versions not available for 8.5
+    # Doing that, we also need to disable the epel-modular repo
+    # as it raises an 404 error
     if "centos-8.5" in SYSTEM_RELEASE_ENV:
-        cmd = "yum check-update --quiet --releasever=8.5 %s"
+        cmd = "yum check-update --quiet --releasever=8.5 --disablerepo epel-modular %s"
     for package in packages_to_verify:
         # If tha package lands on latest version after conversion
         # `yum check-update` will return 0
-        # If the package is possible to update the returncode yields 100
+        # If it is possible to update the package, the yum returncode yields 100
         assert shell(cmd % package).returncode == 0
