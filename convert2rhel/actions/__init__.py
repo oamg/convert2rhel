4930d4bfcb195a70c077479a824540c6b3d7a85c
Preston Watson <prwatson@redhat.com>
2023-08-24 16:53:52 -0400

    Added missing info header

--- convert2rhel/actions/__init__.py
+++ convert2rhel/actions/__init__.py
@@ -83,6 +83,7 @@ _STATUS_NAME_FROM_CODE = dict((value, key) for key, value in STATUS_CODE.items()
 #: what the results mean
 _STATUS_HEADER = {
     0: "Success (No changes needed)",
+    25: "Info (No changes needed)",
     51: "Warning (Review and fix if needed)",
     101: "Skip (Could not be checked due to other failures)",
     152: "Overridable (Review and either fix or ignore the failure)",
