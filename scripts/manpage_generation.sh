8ef55c6d851aa2561124f4e126e56d7c72900e18
Andrew-ang9 <91086569+Andrew-ang9@users.noreply.github.com>
2023-09-20 13:47:46 -0400

    [RHELC-1037] Auto-generate manpages (#876)

---
+++ scripts/manpage_generation.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Directory to store the generated manpages
+MANPAGE_DIR="man"
+
+VER=$(grep -oP '^Version:\s+\K\S+' packaging/convert2rhel.spec)
+
+# Generate a file with convert2rhel synopsis for argparse-manpage
+python -c 'from convert2rhel import toolopts; print("[synopsis]\n."+toolopts.CLI.usage())' > man/synopsis
+
+# Generate the manpage using argparse-manpage
+PYTHONPATH=. argparse-manpage --pyfile man/__init__.py --function get_parser --manual-title="General Commands Manual" --description="Automates the conversion of Red Hat Enterprise Linux derivative distributions to Red Hat Enterprise Linux." --project-name "convert2rhel $VER" --prog="convert2rhel" --include man/distribution --include man/synopsis > "$MANPAGE_DIR/convert2rhel.8"
