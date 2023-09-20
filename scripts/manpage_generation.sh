c160dc26573ee79c90de21583cd0dc20fd134d24
Andrew Anglin <aanglin@redhat.com>
2023-09-20 12:09:37 -0400

    fixed a typo in genreate manpage command

--- scripts/manpage_generation.sh
+++ scripts/manpage_generation.sh
@@ -9,4 +9,4 @@ VER=$(grep -oP '^Version:\s+\K\S+' packaging/convert2rhel.spec)
 python -c 'from convert2rhel import toolopts; print("[synopsis]\n."+toolopts.CLI.usage())' > man/synopsis
 
 # Generate the manpage using argparse-manpage
-PYTHONPATH=. argparse-manpage --pyfile man/__init__.py --function get_parser --manual-title="General Commands Manual" --description="Automates the conversion of Red Hat Enterprise Linux derivative distributions to Red Hat Enterprise Linux." --project-name "convert2rhle $Ver" --prog="convert2rhel" --include man/distribution --include man/synopsis > "$MANPAGE_DIR/convert2rhel.8"
+PYTHONPATH=. argparse-manpage --pyfile man/__init__.py --function get_parser --manual-title="General Commands Manual" --description="Automates the conversion of Red Hat Enterprise Linux derivative distributions to Red Hat Enterprise Linux." --project-name "convert2rhel $Ver" --prog="convert2rhel" --include man/distribution --include man/synopsis > "$MANPAGE_DIR/convert2rhel.8"
