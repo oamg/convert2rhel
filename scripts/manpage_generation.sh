d1497b6a10cde2c247dcd18ed9eaf1725dfd85f9
Andrew Anglin <aanglin@redhat.com>
2023-09-20 11:50:36 -0400

    Added in a command to add the synopsis

--- scripts/manpage_generation.sh
+++ scripts/manpage_generation.sh
@@ -5,5 +5,8 @@ MANPAGE_DIR="man"
 
 VER=$(grep -oP '^Version:\s+\K\S+' packaging/convert2rhel.spec)
 
+# Generate a file with convert2rhel synopsis for argparse-manpage
+python -c 'from convert2rhel import toolopts; print("[synopsis]\n."+toolopts.CLI.usage())' > man/synopsis
+
 # Generate the manpage using argparse-manpage
 PYTHONPATH=. argparse-manpage --pyfile man/__init__.py --function get_parser --manual-title="General Commands Manual" --description="Automates the conversion of Red Hat Enterprise Linux derivative distributions to Red Hat Enterprise Linux." --project-name "convert2rhle $Ver" --prog="convert2rhel" --include man/distribution --include man/synopsis > "$MANPAGE_DIR/convert2rhel.8"
