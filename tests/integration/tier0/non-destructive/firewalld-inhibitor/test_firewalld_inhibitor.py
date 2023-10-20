71c788b6e1e68e464b0fdc10f1659c99094f3ef5
Martin 'kokesak' Litwora <mlitwora@redhat.com>
2023-10-20 13:23:09 +0200

    Add firewalld inhibitor test

---
+++ tests/integration/tier0/non-destructive/firewalld-inhibitor/test_firewalld_inhibitor.py
@@ -0,0 +1,23 @@
+import pytest
+
+
+@pytest.mark.test_firewalld_inhibitor
+def test_firewalld_inhibitor(shell, convert2rhel):
+    """
+    Verify that on the OL8.8 the conversion is inhibited due to
+    running firewalld on the system.
+    The reference ticket: https://issues.redhat.com/browse/RHELC-1180
+    """
+    assert shell("rpm -q firewalld").returncode == 0
+    assert shell("systemctl start firewalld").returncode == 0
+
+    with convert2rhel("-y --no-rpm-va --debug", unregister=True) as c2r:
+        c2r.expect(
+            "CHECK_FIREWALLD_AVAILABILITY::FIREWALLD_DAEMON_RUNNING - Firewalld is running",
+            timeout=600,
+        )
+
+    assert c2r.exitstatus == 1
+
+    # Clean up
+    assert shell("systemctl stop firewalld").returncode == 0
