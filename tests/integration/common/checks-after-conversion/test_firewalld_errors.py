5709cb967e0651d6d2f5c50c7c58d144e64ec9d7
Martin 'kokesak' Litwora <mlitwora@redhat.com>
2023-10-27 18:21:10 +0200

    Add firewalld config integration test

---
+++ tests/integration/common/checks-after-conversion/test_firewalld_errors.py
@@ -0,0 +1,11 @@
+import pytest
+
+
+@pytest.mark.check_firewalld_errors
+def test_firewalld_errors(shell):
+    """Verify that there are no errors in firewalld"""
+    if shell("rpm -q firewalld").returncode == 0:
+        shell("systemctl start firewalld")
+        assert shell("journalctl -u firewalld | grep -i ERROR").returncode == 1
+    else:
+        pytest.skip("Skipping because firewalld package is not present on system.")
--- tests/integration/tier0/non-destructive/firewalld-inhibitor/test_firewalld_inhibitor.py
+++ tests/integration/tier0/non-destructive/firewalld-inhibitor/test_firewalld_inhibitor.py
@@ -2,17 +2,22 @@ import pytest
 
 from envparse import env
 
+from convert2rhel.actions.system_checks.check_firewalld_availability import FIREWALLD_CONFIG_FILE
+
 
 @pytest.mark.test_firewalld_inhibitor
 def test_firewalld_inhibitor(shell, convert2rhel):
     """
     Verify that on the OL8.8 the conversion is inhibited due to
-    running firewalld on the system.
+    running firewalld on the system and the `CleanupModulesOnExit` configuration
+    option is set to `yes` in firewalld configuration file.
     The reference ticket: https://issues.redhat.com/browse/RHELC-1180
     """
     assert shell("rpm -q firewalld").returncode == 0
     assert shell("systemctl start firewalld").returncode == 0
     assert shell("systemctl enable firewalld").returncode == 0
+
+    assert shell(f"grep 'CleanupModulesOnExit=yes' {FIREWALLD_CONFIG_FILE}").returncode == 0
 
     with convert2rhel(
         "-y --no-rpm-va --debug --serverurl {} --username {} --password {}".format(
@@ -20,12 +25,17 @@ def test_firewalld_inhibitor(shell, convert2rhel):
         ),
         unregister=True,
     ) as c2r:
+        c2r.expect("Pre-conversion analysis report")
         c2r.expect(
-            "CHECK_FIREWALLD_AVAILABILITY::FIREWALLD_RUNNING - Firewalld is running",
+            "CHECK_FIREWALLD_AVAILABILITY::FIREWALLD_MODULES_CLEANUP_ON_EXIT_CONFIG",
             timeout=600,
         )
 
     assert c2r.exitstatus == 1
+
+    assert shell(
+        "grep 'Firewalld running on Oracle Linux 8 can lead to a conversion failure' /var/log/convert2rhel/convert2rhel.log"
+    )
 
     # Clean up
     assert shell("systemctl stop firewalld").returncode == 0
