8d6376fa97b3c42599eb5b2ce0c44fea8cb024a9
Michal Bocek <mbocek@redhat.com>
2023-12-08 15:26:30 +0100

    [RHELC-1272] Fix a rollback failure with yum-plugin-local installed (#1001)
    * Fix yumdownloader parsing with yum-plugin-local
    
    When the yum-plugin-local is installed, the yumdownloader output prints
    information about building a local copy of a repository. It prints it
    after the downloaded rpm filename and until now we expected the rpm
    filename to be on the last line of the yumdownloader output.
    
    This change looks for the rpm name on any line of the yumdownloader
    output, not just the last line. Because we always download a single
    package, there should be just one rpm filename present in the output.
    
    This commit also makes sure that whenever we can't find the downloaded
    rpm filename in the yumdownloader call output, we stop the conversion
    because continuing would lead to an incomplete rollback.
    
    * yum-plugin-local: Add an integration test
    
    Reproducer of the issue:
    1. Install yum-plugin-local package and centos-logos on a CentOS-7.9
    system
    2. Run `convert2rhel analyze` and wait for it perform a rollback
    3. The system is missing centos-logos
    
    The new test installs a *-logos package, runs convert2rhel analyze, and
    makes sure the *-logos package is installed after the rollback.
    
    * yum-plugin-local: apply test review suggestions
    
    * Fix parsing "using local copy" in pkg download
    
    * Modify the test
    
    * run the convert2rhel two times to verify the scenario works for both -
    * packages got downloaded and packages are already in the backup dir
    * add fixture to clear the downloaded files
    
    Signed-off-by: Daniel Diblik <ddiblik@redhat.com>
    
    ---------
    
    Signed-off-by: Daniel Diblik <ddiblik@redhat.com>
    Co-authored-by: Daniel Diblik <ddiblik@redhat.com>

--- convert2rhel/utils.py
+++ convert2rhel/utils.py
@@ -651,8 +651,8 @@ def download_pkg(
     :type enable_repos: list[str]
     :param disable_repos: The repositories to disable during the download.
     :type disable_repos: list[str]
-    :param set_systeminfo_releasever: If it's necessary to use the releasever stored in  SystemInfo.releasever.
-    :type set_systeminfo_releasever: bool
+    :param set_releasever: If it's necessary to use the releasever stored in  SystemInfo.releasever.
+    :type set_releasever: bool
     :param custom_releasever: A custom releasever to use. An alternative to set_systeminfo_releasever.
     :type custom_releasever: int | str
     :param varsdir: The path to the variables directory.
@@ -697,86 +697,98 @@ def download_pkg(
 
     output, ret_code = run_cmd_in_pty(cmd, print_output=False)
     if ret_code != 0:
-        loggerinst.warning("Output from the yumdownloader call:\n%s" % (output))
-
-        # Note: Checking toolopts here is a temporary solution. We need to
-        # restructure this to raise an exception on error and have the caller
-        # handle whether to use INCOMPLETE_ROLLBACK to do something for several
-        # reasons:
-        # (1) utils should be simple functions that take input and produce
-        #     output from it. Having knowledge of things specific to the
-        #     program (for instance, the environment variable that convert2rhel
-        #     uses) makes the utils depend on the specific place that they are
-        #     run instead.
-        # (2) Where an error condition arises, they should "return" that to the
-        #     caller to decide how to handle it by using an exception. Handling
-        #     it inside the function ties us to one specific behaviour on
-        #     error. (For instance, the incomplete rollback message here ties
-        #     downloading packages and performing rollbacks. But what about
-        #     downloading packages that are not tied to rollbacks. Maybe we
-        #     have to download a package in order for insights or
-        #     subscription-manager to run. In those cases, we either cannot use
-        #     this function or we might show the user a misleading message).
-        # (3) Functions in utils should be free of other dependencies within
-        #     convert2rhel.  That allows us to use utils with no fear of
-        #     circular dependency issues.
-        # (4) Making the choices here mean that when used inside of the Action
-        #     framework, we are limited to returning a FAILURE for the Action
-        #     plugin whereas returning SKIP would be more accurate.
-        from convert2rhel import toolopts
-
-        if toolopts.tool_opts.activity == "conversion":
-            if "CONVERT2RHEL_UNSUPPORTED_INCOMPLETE_ROLLBACK" not in os.environ:
-                loggerinst.critical(
-                    "Couldn't download the %s package. This means we will not be able to do a"
-                    " complete rollback and may put the system in a broken state.\n"
-                    "Check to make sure that the %s repositories are enabled"
-                    " and the package is updated to its latest version.\n"
-                    "If you would rather ignore this check set the environment variable"
-                    " 'CONVERT2RHEL_UNSUPPORTED_INCOMPLETE_ROLLBACK'." % (pkg, system_info.name)
-                )
-            else:
-                loggerinst.warning(
-                    "Couldn't download the %s package. This means we will not be able to do a"
-                    " complete rollback and may put the system in a broken state.\n"
-                    "'CONVERT2RHEL_UNSUPPORTED_INCOMPLETE_ROLLBACK' environment variable detected, continuing conversion."
-                    % (pkg)
-                )
-            return None
-        else:
-            loggerinst.critical(
-                "Couldn't download the %s package which is needed to do a rollback of this action."
-                " Check to make sure that the %s repositories are enabled and the package is"
-                " updated to its latest version.\n"
-                "Note that you can choose to ignore this check when actually running a conversion by"
-                " setting the environment variable 'CONVERT2RHEL_UNSUPPORTED_INCOMPLETE_ROLLBACK'"
-                " but not during pre-conversion analysis." % (pkg, system_info.name)
-            )
+        report_on_a_download_error(output, pkg)
+        return None
 
     path = get_rpm_path_from_yumdownloader_output(cmd, output, dest)
-    if path:
-        loggerinst.info("Successfully downloaded the %s package." % pkg)
-        loggerinst.debug("Path of the downloaded package: %s" % path)
+    if not path:
+        report_on_a_download_error(output, pkg)
+        return None
+
+    loggerinst.info("Successfully downloaded the %s package." % pkg)
+    loggerinst.debug("Path of the downloaded package: %s" % path)
 
     return path
+
+
+def report_on_a_download_error(output, pkg):
+    """
+    Report on a failure to download a package we need for a complete rollback.
+
+    :param output: Output of the yumdownloader call
+    :param pkg: Name of a package to be downloaded
+    """
+    loggerinst.warning("Output from the yumdownloader call:\n%s" % output)
+
+    # Note: Using toolopts here is a temporary solution. We need to
+    # restructure this to raise an exception on error and have the caller
+    # handle whether to use INCOMPLETE_ROLLBACK to do something for several
+    # reasons:
+    # (1) utils should be simple functions that take input and produce
+    #     output from it. Having knowledge of things specific to the
+    #     program (for instance, the environment variable that convert2rhel
+    #     uses) makes the utils depend on the specific place that they are
+    #     run instead.
+    # (2) Where an error condition arises, they should "return" that to the
+    #     caller to decide how to handle it by using an exception. Handling
+    #     it inside the function ties us to one specific behaviour on
+    #     error. (For instance, the incomplete rollback message here ties
+    #     downloading packages and performing rollbacks. But what about
+    #     downloading packages that are not tied to rollbacks. Maybe we
+    #     have to download a package in order for insights or
+    #     subscription-manager to run. In those cases, we either cannot use
+    #     this function or we might show the user a misleading message).
+    # (3) Functions in utils should be free of other dependencies within
+    #     convert2rhel.  That allows us to use utils with no fear of
+    #     circular dependency issues.
+    # (4) Making the choices here mean that when used inside of the Action
+    #     framework, we are limited to returning a FAILURE for the Action
+    #     plugin whereas returning SKIP would be more accurate.
+    from convert2rhel import toolopts
+    from convert2rhel.systeminfo import system_info
+
+    if toolopts.tool_opts.activity == "conversion":
+        if "CONVERT2RHEL_UNSUPPORTED_INCOMPLETE_ROLLBACK" not in os.environ:
+            loggerinst.critical(
+                "Couldn't download the %s package. This means we will not be able to do a"
+                " complete rollback and may put the system in a broken state.\n"
+                "Check to make sure that the %s repositories are enabled"
+                " and the package is updated to its latest version.\n"
+                "If you would rather ignore this check set the environment variable"
+                " 'CONVERT2RHEL_UNSUPPORTED_INCOMPLETE_ROLLBACK=1'." % (pkg, system_info.name)
+            )
+        else:
+            loggerinst.warning(
+                "Couldn't download the %s package. This means we will not be able to do a"
+                " complete rollback and may put the system in a broken state.\n"
+                "'CONVERT2RHEL_UNSUPPORTED_INCOMPLETE_ROLLBACK=1' environment variable detected, continuing"
+                " conversion." % pkg
+            )
+    else:
+        loggerinst.critical(
+            "Couldn't download the %s package which is needed to do a rollback of this action."
+            " Check to make sure that the %s repositories are enabled and the package is"
+            " updated to its latest version.\n"
+            "Note that you can choose to ignore this check when running a conversion by"
+            " setting the environment variable 'CONVERT2RHEL_UNSUPPORTED_INCOMPLETE_ROLLBACK=1'"
+            " but not during a pre-conversion analysis." % (pkg, system_info.name)
+        )
 
 
 def get_rpm_path_from_yumdownloader_output(cmd, output, dest):
     """Parse the output of yumdownloader to get the filepath of the downloaded rpm.
 
-    The name of the downloaded rpm is on the last line of the output from yumdownloader. The line can look like:
+    The name of the downloaded rpm is in the output of a successful yumdownloader call. The output can look like:
       RHEL 7 & 8: "vim-enhanced-8.0.1763-13.0.1.el8.x86_64.rpm     2.2 MB/s | 1.4 MB     00:00"
       RHEL 7: "using local copy of 7:oraclelinux-release-7.9-1.0.9.el7.x86_64"
       RHEL 8: "[SKIPPED] oraclelinux-release-8.2-1.0.8.el8.x86_64.rpm: Already downloaded"
     """
-    if output:
-        last_output_line = output.splitlines()[-1]
-    else:
+    if not output:
         loggerinst.warning("The output of running yumdownloader is unexpectedly empty. Command:\n%s" % cmd)
         return None
 
-    rpm_name_match = re.search(r"\S*\.rpm", last_output_line)
-    pkg_nevra_match = re.search(r"^using local copy of (?:\d+:)?(.*)$", last_output_line)
+    rpm_name_match = re.search(r"\S*\.rpm", output)
+    pkg_nevra_match = re.search(r"using local copy of (?:\d+:)?(.*)$", output)
 
     if rpm_name_match:
         path = os.path.join(dest, rpm_name_match.group(0))
