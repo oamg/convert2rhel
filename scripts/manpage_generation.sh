99a1a61bf6058b1f97d96887850301b694e78e0c
E Gustavsson <egustavs@redhat.com>
2024-01-02 18:43:42 +0100

    Add manpage generation to all releases (#1011)

--- scripts/manpage_generation.sh
+++ scripts/manpage_generation.sh
@@ -3,12 +3,17 @@
 # Directory to store the generated manpages
 MANPAGE_DIR="man"
 
+echo Generating manpages
+
 VER=$(grep -oP '^Version:\s+\K\S+' packaging/convert2rhel.spec)
 
+echo Generating for version $VER
 # Generate a file with convert2rhel synopsis for argparse-manpage
-/usr/bin/python3.10 -c 'from convert2rhel import toolopts; print("[synopsis]\n."+toolopts.CLI.usage())' > man/synopsis
+/usr/bin/python -c 'from convert2rhel import toolopts; print("[synopsis]\n."+toolopts.CLI.usage())' > man/synopsis
 
-/usr/bin/python3.10 -m pip install argparse-manpage six pexpect
+/usr/bin/python -m pip install argparse-manpage six pexpect
 
 # Generate the manpage using argparse-manpage
-PYTHONPATH=. /usr/bin/python3.10 /home/runner/.local/bin/argparse-manpage --pyfile man/__init__.py --function get_parser --manual-title="General Commands Manual" --description="Automates the conversion of Red Hat Enterprise Linux derivative distributions to Red Hat Enterprise Linux." --project-name "convert2rhel $VER" --prog="convert2rhel" --include man/distribution --include man/synopsis > "$MANPAGE_DIR/convert2rhel.8"
+PYTHONPATH=. /usr/bin/python /home/runner/.local/bin/argparse-manpage --pyfile man/__init__.py --function get_parser --manual-title="General Commands Manual" --description="Automates the conversion of Red Hat Enterprise Linux derivative distributions to Red Hat Enterprise Linux." --project-name "convert2rhel $VER" --prog="convert2rhel" --include man/distribution --include man/synopsis > "$MANPAGE_DIR/convert2rhel.8"
+
+git status
