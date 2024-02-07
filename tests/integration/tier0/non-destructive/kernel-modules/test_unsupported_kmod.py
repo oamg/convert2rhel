ed95148421ae5c603bc4a106c227efc31c2ade53
Daniel Diblik <ddiblik@redhat.com>
2024-02-07 16:18:30 +0100

    Add integration tests for tainted kernel modules
    * validate that the check is now overridable
    
    Signed-off-by: Daniel Diblik <ddiblik@redhat.com>

--- tests/integration/tier0/non-destructive/kernel-modules/test_unsupported_kmod.py
+++ tests/integration/tier0/non-destructive/kernel-modules/test_unsupported_kmod.py
@@ -123,19 +123,28 @@ def test_do_not_error_if_module_is_not_loaded(shell, convert2rhel):
     assert c2r.exitstatus != 0
 
 
+@pytest.fixture()
+def forced_kmods(shell):
+    # Force load the kernel module
+    assert shell("modprobe -f -v bonding").returncode == 0
+    # Check for force loaded modules being flagged FE in /proc/modules
+    assert "(FE)" in shell("cat /proc/modules").output
+
+    yield
+
+    # Clean up - unload kmod and check for force loaded modules not being in /proc/modules
+    assert shell("modprobe -r -v bonding").returncode == 0
+    assert "(FE)" not in shell("cat /proc/modules").output
+
+
 @pytest.mark.test_force_loaded_kmod
-def test_error_if_module_is_force_loaded(shell, convert2rhel):
+def test_error_if_module_is_force_loaded(shell, convert2rhel, forced_kmods):
     """
     In this test case we force load kmod and verify that the convert2rhel raises:
     TAINTED_KMODS.TAINTED_KMODS_DETECTED.
     Force loaded kmods are denoted (FE) where F = module was force loaded E = unsigned module was loaded.
     Convert2RHEL sees force loaded kmod as tainted.
     """
-    # Force load the kernel module
-    assert shell("modprobe -f -v bonding").returncode == 0
-    # Check for force loaded modules being flagged FE in /proc/modules
-    assert "(FE)" in shell("cat /proc/modules").output
-
     with convert2rhel("--debug") as c2r:
         # We need to get past the data collection acknowledgement.
         c2r.expect("Continue with the system conversion?")
@@ -146,13 +155,42 @@ def test_error_if_module_is_force_loaded(shell, convert2rhel):
 
     assert c2r.exitstatus != 0
 
-    # Clean up - unload kmod and check for force loaded modules not being in /proc/modules
-    assert shell("modprobe -r -v bonding").returncode == 0
-    assert "(FE)" not in shell("cat /proc/modules").output
+
+@pytest.mark.parametrize("envars", [["CONVERT2RHEL_TAINTED_KERNEL_MODULE_CHECK_SKIP"]])
+@pytest.mark.test_tainted_kernel_modules_check_skip
+def test_tainted_kernel_modules_check_skip(shell, convert2rhel, forced_kmods, environment_variables, envars):
+    """
+    In this test case we force load kmod and verify that the TAINTED_KMODS.TAINTED_KMODS_DETECTED
+    is overridable by setting the environment variable 'CONVERT2RHEL_TAINTED_KERNEL_MODULE_CHECK_SKIP'
+    to '1'
+    Force loaded kmods are denoted (FE) where F = module was force loaded E = unsigned module was loaded.
+    Convert2RHEL sees force loaded kmod as tainted.
+    """
+    environment_variables(envars)
+    with convert2rhel(
+        "--serverurl {} --username {} --password {} --debug".format(
+            env.str("RHSM_SERVER_URL"),
+            env.str("RHSM_USERNAME"),
+            env.str("RHSM_PASSWORD"),
+        ),
+        unregister=True,
+    ) as c2r:
+        # We need to get past the data collection acknowledgement.
+        c2r.expect("Continue with the system conversion?")
+        c2r.sendline("y")
+        # Validate that with the envar the conversion is not inhibited with an error,
+        # but just a warning is displayed and the user is allowed to proceed with a full conversion
+        c2r.expect_exact("(WARNING) TAINTED_KMODS::SKIP_TAINTED_KERNEL_MODULE_CHECK")
+        c2r.expect_exact("(WARNING) TAINTED_KMODS::TAINTED_KMODS_DETECTED_MESSAGE")
+        # Cancel the conversion, we do not need to get further
+        c2r.expect("Continue with the system conversion?")
+        c2r.sendline("n")
+
+    assert c2r.exitstatus != 0
 
 
-@pytest.mark.test_tainted_kernel
-def test_tainted_kernel_error(custom_kmod, convert2rhel):
+@pytest.mark.test_tainted_kernel_modules_error
+def test_tainted_kernel_modules_error(custom_kmod, convert2rhel):
     """
     This test marks the kernel as tainted which is not supported by convert2rhel.
     We need to install specific kernel packages to build own custom kernel module.
@@ -169,19 +207,25 @@ def test_tainted_kernel_error(custom_kmod, convert2rhel):
         unregister=True,
     ) as c2r:
         c2r.expect("Tainted kernel modules detected")
+        c2r.expect(
+            "disregard this message by setting the environment variable 'CONVERT2RHEL_TAINTED_KERNEL_MODULE_CHECK_SKIP'"
+        )
         c2r.sendcontrol("c")
 
     assert c2r.exitstatus != 0
 
 
+@pytest.mark.parametrize("envars", [["CONVERT2RHEL_ALLOW_UNAVAILABLE_KMODS"]])
 @pytest.mark.test_unsupported_kmod_with_envar
-def test_envar_overrides_unsupported_module_loaded(kmod_in_different_directory, convert2rhel):
+def test_envar_overrides_unsupported_module_loaded(
+    kmod_in_different_directory, convert2rhel, environment_variables, envars
+):
     """
     This test verifies that setting the environment variable "CONVERT2RHEL_ALLOW_UNAVAILABLE_KMODS"
     will override the check error when there is RHEL unsupported kernel module detected.
     The environment variable is set through the test metadata.
     """
-
+    environment_variables(envars)
     with convert2rhel(
         "--serverurl {} --username {} --password {} --pool {} --debug".format(
             env.str("RHSM_SERVER_URL"),
@@ -199,6 +243,3 @@ def test_envar_overrides_unsupported_module_loaded(kmod_in_different_directory, 
         c2r.sendcontrol("c")
 
     assert c2r.exitstatus != 0
-
-    # Remove the set environment variable
-    del os.environ["CONVERT2RHEL_ALLOW_UNAVAILABLE_KMODS"]
