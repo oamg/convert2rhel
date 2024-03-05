31f5447a5c94733fbdc0209435a8c961147da1d4
Michal Bocek <mbocek@redhat.com>
2024-03-05 23:05:34 +0100

    Download c2r repofile instead of generating it
    It's safer to use the official convert2rhel repofile that is to be used
    by customers instead of generating it as that way any changes done it
    don't need further convert2rhel code updates.

--- tests/integration/tier0/non-destructive/subscription-manager/test_sub_man_rollback.py
+++ tests/integration/tier0/non-destructive/subscription-manager/test_sub_man_rollback.py
@@ -14,10 +14,10 @@ def test_sub_man_rollback(convert2rhel, shell, required_packages):
     Not removing and backing up the python3-syspurpose and python3-cloud-what packages on CentOS Linux 8.5 was causing
     a rollback failure when an older version of these two packages was installed.
 
-    The rollback failure caused the following issue during the subsequent second run of convert2rhel:
-      When the rollback happened before removing the centos-linux-release package, this package was not re-installed
-      back during the rollback and that lead to the $releasever variable being undefined, ultimately causing a traceback
-      when using the DNF python API (https://issues.redhat.com/browse/RHELC-762)
+    When a reinstallation of removed packages failed during the rollback due to not having the right versions of updated
+    dependencies backed up, the system was left without the centos-linux-release package installed, leading to the
+    $releasever variable being undefined, and ultimately causing a traceback during a subsequent execution of
+    convert2rhel (https://issues.redhat.com/browse/RHELC-762).
     """
     # By running convert2rhel twice we make sure that the rollback of the first run
     # correctly reinstalled centos-linux-release and subscription-manager* and the second run then does not fail
