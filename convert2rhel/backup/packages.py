d0e84674c96388c62309fdb098979297c941cc55
Freya Gustavsson <freya@spytec.se>
2024-03-05 17:38:05 +0000

    refactor(utils): create utils module with mkdir_p
    As we kept using `utils.py` file and adding more and more things it
    became quite bloated. To solve this the best thing to do, while also
    making it easy to test and reduce cyclic imports is to split it into
    smaller parts.
    
    This takes care of the initial move by putting everything on the module
    level while still having backwards compatibility for the time being.
    
    To make this easier for ourselves we should migrate the file into
    more modules over time.
    
    To start with, this changes the `mkdir_p()` function to be under utils
    module under `files.py`.

--- convert2rhel/backup/packages.py
+++ convert2rhel/backup/packages.py
@@ -28,6 +28,7 @@ from convert2rhel.pkgmanager import call_yum_cmd
 # split this out.
 from convert2rhel.repo import get_hardcoded_repofiles_dir
 from convert2rhel.systeminfo import system_info
+from convert2rhel.utils import files
 
 
 loggerinst = logging.getLogger(__name__)
@@ -308,8 +309,8 @@ class RestorablePackageSet(RestorableChange):
         # Note, this use of mkdir_p is secure because SUBMGR_RPMS_DIR and
         # _RHSM_TMP_DIR do not contain any path components writable by
         # a different user.
-        utils.mkdir_p(_SUBMGR_RPMS_DIR)
-        utils.mkdir_p(_RHSM_TMP_DIR)
+        files.mkdir_p(_SUBMGR_RPMS_DIR)
+        files.mkdir_p(_RHSM_TMP_DIR)
 
         loggerinst.info("Downloading requested packages")
         all_pkgs_to_install = self.pkgs_to_install + self.pkgs_to_update
