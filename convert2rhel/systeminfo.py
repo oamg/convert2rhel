9660a847cec349af2f01f706fb905529fddc3872
Preston Watson <prwatson@redhat.com>
2024-04-30 11:29:05 -0400

    Add rhel 9 version support up to 9.10

--- convert2rhel/systeminfo.py
+++ convert2rhel/systeminfo.py
@@ -38,6 +38,14 @@ CHECK_DBUS_STATUS_RETRIES = 3
 # Allowed conversion paths to RHEL. We want to prevent a conversion and minor
 # version update at the same time.
 RELEASE_VER_MAPPING = {
+    "9.10": "9.10",
+    "9.9": "9.9",
+    "9.8": "9.8",
+    "9.7": "9.7",
+    "9.6": "9.6",
+    "9.5": "9.5",
+    "9.4": "9.4",
+    "9.3": "9.3",
     "9.2": "9.2",
     "9.1": "9.1",
     "9.0": "9.0",
