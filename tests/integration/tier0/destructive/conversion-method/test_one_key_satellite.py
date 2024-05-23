a3873b97b6780d83c67a1e7f3decd62ea621a501
Daniel Diblik <ddiblik@redhat.com>
2024-05-23 15:08:11 +0200

    Enable original system repos pre-conversion
    * the repositories are not enabled by default, we need to enable them
      for convert2rhel to be able to perform the checks and backups
    
    Signed-off-by: Daniel Diblik <ddiblik@redhat.com>

--- tests/integration/tier0/destructive/conversion-method/test_one_key_satellite.py
+++ tests/integration/tier0/destructive/conversion-method/test_one_key_satellite.py
@@ -1,7 +1,7 @@
 import pytest
 
 
-@pytest.mark.parametrize("satellite_registration", ["RHEL7_AND_CENTOS7_SAT_REG"])
+@pytest.mark.parametrize("satellite_registration", ["RHEL7_AND_CENTOS7_SAT_REG"], indirect=True)
 @pytest.mark.test_one_key_satellite_conversion
 def test_one_key_satellite_conversion(shell, convert2rhel, satellite_registration, remove_repositories):
     """
@@ -9,6 +9,9 @@ def test_one_key_satellite_conversion(shell, convert2rhel, satellite_registratio
     The system is pre-registered to the Satellite instance prior to the conversion.
     We use one activation key containing both the original OS and RHEL repositories.
     """
+    # We need to enable the current system repositories prior to the conversion
+    shell("subscription-manager repos --enable=*")
+
     with convert2rhel("-y --debug") as c2r:
         c2r.expect("Conversion successful!")
     assert c2r.exitstatus == 0
