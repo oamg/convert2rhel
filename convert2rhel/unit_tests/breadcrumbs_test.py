0f8121c7c6b087fd968aac90c25e6360c4e3b410
Andrew Anglin <aanglin@redhat.com>
2024-06-12 13:56:00 -0400

    Made a function for the user back up check
    * now the prompt to ask the user if they have a back up of there system
      is in a function
    * draft the unit test for this function

--- convert2rhel/unit_tests/breadcrumbs_test.py
+++ convert2rhel/unit_tests/breadcrumbs_test.py
@@ -18,13 +18,17 @@
 __metaclass__ = type
 
 import json
+import sys
 
 import pytest
 import six
 
+import convert2rhel.toolopts
+
 from convert2rhel import breadcrumbs, pkghandler, pkgmanager
 from convert2rhel.unit_tests import create_pkg_information, create_pkg_obj
 from convert2rhel.unit_tests.conftest import centos7
+from convert2rhel.unit_tests.toolopts_test import mock_cli_arguments
 
 
 six.add_move(six.MovedModule("mock", "mock", "unittest.mock"))
@@ -330,3 +334,28 @@ def test_set_target_os(pretend_os):
         "name": "CentOS Linux",
         "version": "7.9",
     } == breadcrumbs.breadcrumbs.target_os
+
+
+@pytest.mark.parametrize(
+    ("argv", "expected", "message"),
+    (
+        (
+            ["analyze"],
+            False,
+            "Convert2RHEL modifies the systems during the analysis and then rolls back these "
+            "changes when the analysis is complete. In rare cases, this rollback can fail. "
+            "By continuing, you confirm that you have made a system backup and verified that "
+            "you can restore from the backup.",
+        ),
+    ),
+)
+def test_confirm_user_backup(argv, expected, message, monkeypatch, global_tool_opts, caplog):
+    monkeypatch.setattr(sys, "argv", mock_cli_arguments(argv))
+    try:
+        convert2rhel.toolopts.CLI()
+    except SystemExit:
+        pass
+
+    # assert global_tool_opts.no_rpm_va == expected
+    if message:
+        assert message in caplog.text
