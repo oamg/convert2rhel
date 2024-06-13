3f607e818797ca2363804fec93c2ff67c284ef8f
Daniel Diblik <ddiblik@redhat.com>
2024-06-13 14:07:31 +0200

    Add a new check, fix tests, remove remediation
    * Add a new post-coversion check verifying default grub entry and
      correct running kernel
    * fix broken test, where /etc/yum.repos.d got removed
    * remove python3-syspurpose outdated remediation to validate RHELC-1389
      is resolved
    
    Signed-off-by: Daniel Diblik <ddiblik@redhat.com>

--- tests/integration/tier0/destructive/conversion-method/test_custom_repos.py
+++ tests/integration/tier0/destructive/conversion-method/test_custom_repos.py
@@ -1,6 +1,6 @@
 import pytest
 
-from conftest import SystemInformationRelease
+from conftest import SYSTEM_RELEASE_ENV, SystemInformationRelease
 
 
 @pytest.mark.test_custom_repos_conversion
@@ -12,33 +12,32 @@ def test_run_conversion_using_custom_repos(shell, convert2rhel):
     The repositories enabled in this scenario are
     {rhel7: [server rpms, extras rpms, optional rpms], rhel8: [[eus-]?baseos], [eus-]?appstream}.
     """
+    system_version = SystemInformationRelease.version
+    system_distribution = SystemInformationRelease.distribution
+    enable_repo_opt = "--enablerepo rhel-7-server-rpms --enablerepo rhel-7-server-optional-rpms --enablerepo rhel-7-server-extras-rpms"
 
-    with open("/etc/system-release", "r") as file:
-        system_version = SystemInformationRelease.version
-        enable_repo_opt = "--enablerepo rhel-7-server-rpms --enablerepo rhel-7-server-optional-rpms --enablerepo rhel-7-server-extras-rpms"
-
-        if system_version.major == 8:
-            if system_version.minor == 8:
-                enable_repo_opt = (
-                    "--enablerepo rhel-8-for-x86_64-baseos-eus-rpms --enablerepo rhel-8-for-x86_64-appstream-eus-rpms"
-                )
-                # Mark the system so the check for the enabled repos after the conversion handles this special case
-                shell("touch /eus_repos_used")
-            else:
-                enable_repo_opt = (
-                    "--enablerepo rhel-8-for-x86_64-baseos-rpms --enablerepo rhel-8-for-x86_64-appstream-rpms"
-                )
-        if system_version.major == 9:
-            if system_version.minor in (4, 6, 8):
-                enable_repo_opt = (
-                    "--enablerepo rhel-9-for-x86_64-baseos-eus-rpms --enablerepo rhel-9-for-x86_64-appstream-eus-rpms"
-                )
-                # Mark the system so the check for the enabled repos after the conversion handles this special case
-                shell("touch /eus_repos_used")
-            else:
-                enable_repo_opt = (
-                    "--enablerepo rhel-9-for-x86_64-baseos-rpms --enablerepo rhel-9-for-x86_64-appstream-rpms"
-                )
+    if system_version.major == 8:
+        if system_version.minor == 8:
+            enable_repo_opt = (
+                "--enablerepo rhel-8-for-x86_64-baseos-eus-rpms --enablerepo rhel-8-for-x86_64-appstream-eus-rpms"
+            )
+            # Mark the system so the check for the enabled repos after the conversion handles this special case
+            shell("touch /eus_repos_used")
+        else:
+            enable_repo_opt = "--enablerepo rhel-8-for-x86_64-baseos-rpms --enablerepo rhel-8-for-x86_64-appstream-rpms"
+    if system_version.major == 9:
+        # We want to go for EUS repositories only where really applicable
+        #   1/ minor version matches EUS eligible version
+        #   2/ the system distribution does snapshots of said versions
+        #   3/ the current version is not the latest one available (i.e. not in the EUS phase yet)
+        if system_version.minor in (4, 6, 8) and system_distribution != "oracle" and "latest" not in SYSTEM_RELEASE_ENV:
+            enable_repo_opt = (
+                "--enablerepo rhel-9-for-x86_64-baseos-eus-rpms --enablerepo rhel-9-for-x86_64-appstream-eus-rpms"
+            )
+            # Mark the system so the check for the enabled repos after the conversion handles this special case
+            shell("touch /eus_repos_used")
+        else:
+            enable_repo_opt = "--enablerepo rhel-9-for-x86_64-baseos-rpms --enablerepo rhel-9-for-x86_64-appstream-rpms"
 
     with convert2rhel("-y --no-rhsm {} --debug".format(enable_repo_opt)) as c2r:
         c2r.expect("Conversion successful!")
@@ -55,7 +54,11 @@ def test_run_conversion_using_custom_repos(shell, convert2rhel):
         else:
             enable_repo_opt = "--enable rhel-8-for-x86_64-baseos-rpms --enable rhel-8-for-x86_64-appstream-rpms"
     elif system_version.major == 9:
-        if system_version.minor in (4, 6, 8):
+        # We want to go for EUS repositories only where really applicable
+        #   1/ minor version matches EUS eligible version
+        #   2/ the system distribution does snapshots of said versions
+        #   3/ the current version is not the latest one available (i.e. not in the EUS phase yet)
+        if system_version.minor in (4, 6, 8) and system_distribution != "oracle" and "latest" not in SYSTEM_RELEASE_ENV:
             enable_repo_opt = "--enable rhel-9-for-x86_64-baseos-eus-rpms --enable rhel-9-for-x86_64-appstream-eus-rpms"
         else:
             enable_repo_opt = "--enable rhel-9-for-x86_64-baseos-rpms --enable rhel-9-for-x86_64-appstream-rpms"
