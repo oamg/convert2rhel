28a2a4d682c21fa75bde9e86824968890ec44243
Joseph Chapman <jochapma@redhat.com>
2024-07-05 12:46:34 -0400

    Fix wrong module name, add unit test.

--- convert2rhel/unit_tests/actions/conversion/list_non_red_hat_pkgs_left_test.py
+++ convert2rhel/unit_tests/actions/conversion/list_non_red_hat_pkgs_left_test.py
@@ -16,20 +16,26 @@
 __metaclass__ = type
 
 import logging
+import os
 
-from convert2rhel import actions, pkghandler
+import pytest
+import six
 
 
-loggerinst = logging.getLogger(__name__)
+six.add_move(six.MovedModule("mock", "mock", "unittest.mock"))
+from six.moves import mock
+
+from convert2rhel import pkghandler, unit_tests
+from convert2rhel.actions.conversion import list_non_red_hat_pkgs_left
 
 
-class ListNonRedHatPkgsLeft(actions.Action):
-    id = "LIST_NON_RED_HAT_PKGS_LEFT"
-    dependencies = ()  # XXX
+@pytest.fixture
+def list_non_red_hat_pkgs_left_instance():
+    return list_non_red_hat_pkgs_left.ListNonRedHatPkgsLeft()
 
-    def run(self):
-        """List all the packages that have not been replaced by the
-        Red Hat-signed ones during the conversion.
-        """
-        super(ListNonRedHatPkgsLeft, self).run()
-        pkghandler.list_non_red_hat_pkgs_left()
+
+def test_list_non_red_hat_pkgs_left(list_non_red_hat_pkgs_left_instance, monkeypatch):
+    list_non_red_hat_pkgs_left_mock = mock.Mock()
+    monkeypatch.setattr(pkghandler, "list_non_red_hat_pkgs_left", list_non_red_hat_pkgs_left_mock)
+    list_non_red_hat_pkgs_left_instance.run()
+    assert list_non_red_hat_pkgs_left_mock.call_count == 1
