659b95aa6ff591f3504868239d79124ef0b83371
Preston Watson <prwatson@redhat.com>
2024-07-24 14:55:29 -0400

    Add pkg manager config and unit test

---
+++ convert2rhel/actions/conversion/pkg_manager_config.py
@@ -0,0 +1,37 @@
+# Copyright(C) 2024 Red Hat, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+__metaclass__ = type
+
+import logging
+
+from convert2rhel import actions, redhatrelease
+
+
+logger = logging.getLogger(__name__)
+
+
+class PkgManagerConfig(actions.Action):
+    id = "PKG_MANAGER_CONFIG"
+
+    def run(self):
+        """
+        Check if the distoverpkg tag inside the package manager config has been modified before the conversion and if so
+        comment it out and write to the file.
+        """
+        super(PkgManagerConfig, self).run()
+
+        logger.task("Convert: Patch package manager configuration file")
+        redhatrelease.PkgManagerConf.patch()
---
+++ convert2rhel/unit_tests/actions/conversion/pkg_manager_config_test.py
@@ -0,0 +1,38 @@
+# Copyright(C) 2024 Red Hat, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+__metaclass__ = type
+import pytest
+import six
+
+from convert2rhel import redhatrelease, unit_tests
+from convert2rhel.actions.conversion import pkg_manager_config
+
+
+six.add_move(six.MovedModule("mock", "mock", "unittest.mock"))
+from six.moves import mock
+
+
+@pytest.fixture
+def pkg_manager_config_instance():
+    return pkg_manager_config.PkgManagerConfig()
+
+
+def test_pkg_manager_config(pkg_manager_config_instance, monkeypatch):
+    redhat_release_mock = mock.Mock()
+    monkeypatch.setattr(redhatrelease.PkgManagerConf, "patch", redhat_release_mock)
+    pkg_manager_config_instance.run()
+
+    assert redhat_release_mock.call_count == 1
