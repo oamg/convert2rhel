b5faa30ed5da660542905d1067e8a9cd835efaf2
Andrew Anglin <aanglin@redhat.com>
2024-08-05 13:11:49 -0400

    Draft changed the way this installs podman
    * added in the path to make sure that the path used has the right
      permissions

--- enter_manpage_gen_container.sh
+++ enter_manpage_gen_container.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
+# Define the installation directory
+INSTALL_DIR="$HOME/.local/bin"
+
+# Create the directory if it doesn't exist
+mkdir -p $INSTALL_DIR
+
+# Add the installation directory to PATH
+export PATH=$INSTALL_DIR:$PATH
+
+# Check if podman is installed
 if ! command -v podman &> /dev/null
 then
     echo "Podman could not be found, installing..."
-    apt-get update && apt-get install -y podman
+    # Download and install podman
+    curl -L https://github.com/containers/podman/releases/download/v4.0.0/podman-remote-static.tar.gz | tar -xz -C $INSTALL_DIR
 fi
 
+# Run the container
 podman run --rm $(podman build -q -f Containerfiles/manpage.Containerfile)
