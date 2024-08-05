d338ba72b4c53b964252ed304ef0d4f19ed081bb
Andrew Anglin <aanglin@redhat.com>
2024-08-05 13:06:23 -0400

    Draft removed sudo from the podman install command

--- enter_manpage_gen_container.sh
+++ enter_manpage_gen_container.sh
@@ -3,7 +3,7 @@
 if ! command -v podman &> /dev/null
 then
     echo "Podman could not be found, installing..."
-    sudo apt-get update && sudo apt-get install -y podman
+    apt-get update && apt-get install -y podman
 fi
 
 podman run --rm $(podman build -q -f Containerfiles/manpage.Containerfile)
