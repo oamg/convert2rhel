526a48f5070bd4c55d81d219aaf945683feffb22
Joseph Chapman <33102029+jochapma@users.noreply.github.com>
2024-08-08 10:44:05 -0400

    Update convert2rhel/actions/conversion/list_non_red_hat_pkgs_left.py
    Co-authored-by: Rodolfo Olivieri <rodolfo.olivieri3@gmail.com>

--- convert2rhel/actions/conversion/list_non_red_hat_pkgs_left.py
+++ convert2rhel/actions/conversion/list_non_red_hat_pkgs_left.py
@@ -27,7 +27,6 @@ loggerinst = logging.getLogger(__name__)
 
 class ListNonRedHatPkgsLeft(actions.Action):
     id = "LIST_NON_RED_HAT_PKGS_LEFT"
-    dependencies = ()
 
     def run(self):
         """List all the packages that have not been replaced by the
