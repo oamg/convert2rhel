f6bebbabc79f2951ee2617a02b2cbc57f236edba
Adam Hošek <hosek.adam@outlook.com>
2024-08-15 14:24:18 +0200

    Add missing task title in ListNonRedHatPkgsLeft (#1341)

--- convert2rhel/actions/conversion/list_non_red_hat_pkgs_left.py
+++ convert2rhel/actions/conversion/list_non_red_hat_pkgs_left.py
@@ -33,6 +33,7 @@ class ListNonRedHatPkgsLeft(actions.Action):
         Red Hat-signed ones during the conversion.
         """
         super(ListNonRedHatPkgsLeft, self).run()
+        loggerinst.task("Convert: List remaining non-Red Hat packages")
         loggerinst.info("Listing packages not signed by Red Hat")
         non_red_hat_pkgs = get_installed_pkgs_w_different_fingerprint(system_info.fingerprints_rhel)
         if not non_red_hat_pkgs:
