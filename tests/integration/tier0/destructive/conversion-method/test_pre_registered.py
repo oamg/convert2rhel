e9ec9d08152b0d97423d0c9f1598a70931fbc4b2
Andrew Anglin <aanglin@redhat.com>
2024-09-24 17:38:24 -0400

    added some more fixes for the intergration tests

--- tests/integration/tier0/destructive/conversion-method/test_pre_registered.py
+++ tests/integration/tier0/destructive/conversion-method/test_pre_registered.py
@@ -1,4 +1,7 @@
 def test_pre_registered_system_conversion(convert2rhel, pre_registered):
     with convert2rhel("-y --debug") as c2r:
         c2r.expect("Conversion successful!")
+
+        c2r.expect("Continue with the system conversion?")
+        c2r.sendline("y")
     assert c2r.exitstatus == 0
